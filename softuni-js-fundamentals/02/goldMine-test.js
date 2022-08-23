0008af516a7a0db42db5ef661742a9e5a4a243de
Miroslav Krastanov <miroslav.a.krastanov@gmail.com>
2022-08-23 14:39:13 +0300

    fix directory tree

---
+++ softuni-js-fundamentals/02/goldMine-test.js
@@ -0,0 +1,36 @@
+function goldMine(input) {
+    let index = 0;
+    let locations = Number(input[index]);
+    index++;
+
+    for (let i = 1; i <= locations; i++) {
+        let dailyTotal = 0;
+        let dailyTargetAvg = Number(input[index]);
+        index++;
+        let daysPerLoc = Number(input[index]);
+        index++;
+        for (let j = 1; j <= daysPerLoc; j++) {
+            let dailyGoldActual = Number(input[index]);
+            index++;
+            dailyTotal += dailyGoldActual;
+        }
+        let dailyAvgActual = dailyTotal / daysPerLoc;
+        if (dailyAvgActual >= dailyTargetAvg) {
+            console.log(`Good job! Average gold per day: ${dailyAvgActual.toFixed(2)}.`);
+        } else {
+            console.log(`You need ${(dailyTargetAvg - dailyAvgActual).toFixed(2)} gold.`);
+        }
+    }
+
+}
+
+goldMine(["2",
+    "10",
+    "3",
+    "10",
+    "10",
+    "11",
+    "20",
+    "2",
+    "20",
+    "10"]);
