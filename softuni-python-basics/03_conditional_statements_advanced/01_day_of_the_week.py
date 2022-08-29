4ce664bf8d2ccf7342eeb3ec46dbb7aa0b5d9d20
Miroslav Krastanov <miroslav.a.krastanov@gmail.com>
2022-08-29 21:26:17 +0300

    commit solutions

---
+++ softuni-python-basics/03_conditional_statements_advanced/01_day_of_the_week.py
@@ -0,0 +1,19 @@
+day_input = int(input())
+
+if day_input == 1:
+    print("Monday")
+elif day_input == 2:
+    print("Tuesday")
+elif day_input == 3:
+    print("Wednesday")
+elif day_input == 4:
+    print("Thursday")
+elif day_input == 5:
+    print("Friday")
+elif day_input == 6:
+    print("Saturday")
+elif day_input == 7:
+    print("Sunday")
+else:
+    print("Error")
+
