4bc7bad25f0899de4ff62bc0ac2930c84cbc416c
Miroslav Krastanov <miroslav.a.krastanov@gmail.com>
2022-08-30 11:21:09 +0300

    commit solution

---
+++ softuni-python-basics/03_conditional_statements_advanced/06_num_in_range.py
@@ -0,0 +1,7 @@
+number = float(input())
+
+if (number >= -100) and (number <= 100) and (number != 0):
+    print("Yes")
+else:
+    print("No")
+
---
+++ softuni-python-basics/03_conditional_statements_advanced/test.py
@@ -0,0 +1,11 @@
+number = 15
+valid = 10 <= number <= 20
+if valid:
+    print("Invalid")
+elif not valid:
+    print("Valid")
+# below is the same as above - conditional statements
+if valid == True:
+    print("Invalid")
+elif valid != True:
+    print("Valid")
