12bec1e697227d7f4cf10897cde118998a3e9946
Miroslav Krastanov <miroslav.a.krastanov@gmail.com>
2022-09-04 20:38:59 +0300

    commit solution

--- softuni-js-basics/practice-drawing-figures-with-loops/04-triangle-of-dollars.js
+++ softuni-js-basics/practice-drawing-figures-with-loops/04-triangle-of-dollars.js
@@ -1,6 +1,15 @@
 function triangleOfDollars(input) {
-    let 
-
+    let n = Number(input);
+    for (let row = 1; row <= n; row++) {
+        let result = "";
+        for (let col = 1; col <= row; col++) {
+            let dollar = "$ "
+            result += dollar
+            if (col == row) {
+                console.log(result);
+            }
+        }
+    }
 
 }
 
