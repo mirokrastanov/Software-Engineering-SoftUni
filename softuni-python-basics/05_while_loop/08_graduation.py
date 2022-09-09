4427fc9c7f42e225797d1ce542f2677e45c8edcc
Miroslav Krastanov <miroslav.a.krastanov@gmail.com>
2022-09-09 13:51:17 +0300

    code update / final solution

--- softuni-python-basics/05_while_loop/08_graduation.py
+++ softuni-python-basics/05_while_loop/08_graduation.py
@@ -1,20 +1,21 @@
 student_name = input()
-yearly_grade = float(input())
-year = 1
+year = 0
 fail_counter = 0
 total_sum = 0
 gpa = 0
 
 while year <= 12:
-    total_sum += yearly_grade
-    if yearly_grade < 4 and fail_counter < 1:
+    yearly_grade = float(input())
+    year += 1
+    if yearly_grade < 4:
         fail_counter += 1
-    elif yearly_grade < 4 and fail_counter >= 1:
-        print(f"{student_name} has been excluded at {year - 1} grade")
-        break
-    gpa = total_sum / year
+        if fail_counter == 2:
+            print(f"{student_name} has been excluded at {year} grade")
+            break
+        year -= 1
+    else:
+        total_sum += yearly_grade
     if year == 12:
+        gpa = total_sum / 12
         print(f"{student_name} graduated. Average grade: {gpa:.2f}")
         break
-    year += 1
-    yearly_grade = float(input())
