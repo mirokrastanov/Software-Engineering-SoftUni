aefc7a9efbadaf6cc237be2b7f525b7ced1b7689
Miroslav Krastanov <miroslav.a.krastanov@gmail.com>
2022-09-15 18:43:10 +0300

    commit solution

--- softuni-java-basics/T01-first-steps-in-coding/src/P06ConcatenateData.java
+++ softuni-java-basics/T01-first-steps-in-coding/src/P06ConcatenateData.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class P06ConcatenateData {
+import java.util.Scanner;
+
+public class P06ConcatenateData {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String firstName = scanner.nextLine();
+        String lastName = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+        String city = scanner.nextLine();
+
+//        System.out.println("You are " + firstName + " " + lastName + ", a " + age + "-years old person from " + city + ".");
+        System.out.printf("You are %s %s, a %d-years old person from %s.", firstName, lastName, age, city);
+
+    }
 }
