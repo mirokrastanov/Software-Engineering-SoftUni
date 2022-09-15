922d3e2b152d3861205dd1153e431d37cea161ed
Miroslav Krastanov <miroslav.a.krastanov@gmail.com>
2022-09-15 15:07:10 +0300

    commit solution

---
+++ softuni-js-basics/04-for-loop/exercises-optional/10-clock-p2.js
@@ -0,0 +1,12 @@
+function clock2(input) {
+    for (let hour = 0; hour <= 23; hour++) {
+        for (let min = 0; min <= 59; min++) {
+            for (let sec = 0; sec <= 59; sec++) {
+                console.log(`${hour} : ${min} : ${sec}`);
+            }
+        }
+    }
+
+}
+
+clock2();
