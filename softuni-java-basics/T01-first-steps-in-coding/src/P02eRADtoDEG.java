952806de2a59d5480de646031aa393b11a8c3a85
Miroslav Krastanov <miroslav.a.krastanov@gmail.com>
2022-09-16 12:47:32 +0300

    commit solution

--- softuni-java-basics/T01-first-steps-in-coding/src/P02eRADtoDEG.java
+++ softuni-java-basics/T01-first-steps-in-coding/src/P02eRADtoDEG.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class P02eRADtoDEG {
+import java.util.Scanner;
+
+public class P02eRADtoDEG {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double radians = Double.parseDouble(scanner.nextLine());
+        double degrees = radians * 180 / Math.PI;
+
+        System.out.println(degrees);
+    }
 }
