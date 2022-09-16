fad161210b35c82705bd8f4403388c1412f63939
Miroslav Krastanov <miroslav.a.krastanov@gmail.com>
2022-09-16 12:48:50 +0300

    commit solution

--- softuni-java-basics/T01-first-steps-in-coding/src/P08PetShop.java
+++ softuni-java-basics/T01-first-steps-in-coding/src/P08PetShop.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class P08PetShop {
+import java.util.Scanner;
+
+public class P08PetShop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int dogFood = Integer.parseInt(scanner.nextLine());
+        int catFood = Integer.parseInt(scanner.nextLine());
+
+        double cost = catFood * 4.00 + dogFood * 2.50;
+
+        System.out.printf("%.1f lv.%n", cost);
+
+    }
 }
