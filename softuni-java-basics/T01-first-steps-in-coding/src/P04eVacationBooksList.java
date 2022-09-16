6bd126d52f29e5e3afc39687c59449c8397f68c0
Miroslav Krastanov <miroslav.a.krastanov@gmail.com>
2022-09-16 12:47:44 +0300

    commit solution

--- softuni-java-basics/T01-first-steps-in-coding/src/P04eVacationBooksList.java
+++ softuni-java-basics/T01-first-steps-in-coding/src/P04eVacationBooksList.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class P04eVacationBooksList {
+import java.util.Scanner;
+
+public class P04eVacationBooksList {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int pagesPerBook = Integer.parseInt(scanner.nextLine());
+        int pagesPerHour = Integer.parseInt(scanner.nextLine());
+        int daysToRead = Integer.parseInt(scanner.nextLine());
+
+        int pagesPerDayToFinish = (pagesPerBook / daysToRead) / pagesPerHour;
+
+        System.out.println(pagesPerDayToFinish);
+    }
 }
