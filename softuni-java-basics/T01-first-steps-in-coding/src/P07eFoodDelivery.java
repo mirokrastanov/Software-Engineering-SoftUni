2451cc03e287e3be28a8883c1159e4c1b11e0dec
Miroslav Krastanov <miroslav.a.krastanov@gmail.com>
2022-09-16 12:48:11 +0300

    commit solution

--- softuni-java-basics/T01-first-steps-in-coding/src/P07eFoodDelivery.java
+++ softuni-java-basics/T01-first-steps-in-coding/src/P07eFoodDelivery.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class P07eFoodDelivery {
+import java.util.Scanner;
+
+public class P07eFoodDelivery {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int chickenMeal = Integer.parseInt(scanner.nextLine());
+        int fishMeal = Integer.parseInt(scanner.nextLine());
+        int veganMeal = Integer.parseInt(scanner.nextLine());
+        double total = 1.2 * (chickenMeal * 10.35 + fishMeal * 12.40 + veganMeal * 8.15) + 2.50;
+
+        System.out.println(total);
+    }
 }
