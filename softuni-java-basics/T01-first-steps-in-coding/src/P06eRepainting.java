5856619728b54517ae1b72a788d5672fadfa684d
Miroslav Krastanov <miroslav.a.krastanov@gmail.com>
2022-09-16 12:48:03 +0300

    commit solution

--- softuni-java-basics/T01-first-steps-in-coding/src/P06eRepainting.java
+++ softuni-java-basics/T01-first-steps-in-coding/src/P06eRepainting.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class P06eRepainting {
+import java.util.Scanner;
+
+public class P06eRepainting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int nylon = Integer.parseInt(scanner.nextLine());
+        int paint = Integer.parseInt(scanner.nextLine());
+        int diluent = Integer.parseInt(scanner.nextLine());
+        int hoursReq = Integer.parseInt(scanner.nextLine());
+        double rawCost = (nylon + 2) * 1.50 + paint * 14.50 * 1.1 + diluent * 5.00 + 0.40;
+        double workersPay = rawCost * 0.30 * hoursReq;
+        double totalCost = rawCost + workersPay;
+
+        System.out.println(totalCost);
+    }
 }
