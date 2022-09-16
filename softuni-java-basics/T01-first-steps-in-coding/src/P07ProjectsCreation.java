f1076324c32b062e5bd5c4287ea87cc4549587bd
Miroslav Krastanov <miroslav.a.krastanov@gmail.com>
2022-09-16 12:48:15 +0300

    commit solution

--- softuni-java-basics/T01-first-steps-in-coding/src/P07ProjectsCreation.java
+++ softuni-java-basics/T01-first-steps-in-coding/src/P07ProjectsCreation.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class P07ProjectsCreation {
+import java.util.Scanner;
+
+public class P07ProjectsCreation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String architectName = scanner.nextLine();
+        int projectsToDo = Integer.parseInt(scanner.nextLine());
+        int result = projectsToDo * 3;
+
+        System.out.printf("The architect %s will need %d hours to complete %d project/s.%n", architectName, result, projectsToDo);
+
+
+    }
 }
