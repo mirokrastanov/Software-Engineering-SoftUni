08a43998ca7e18818b0b94b9686e01026e6b3524
Miroslav Krastanov <miroslav.a.krastanov@gmail.com>
2022-09-16 12:48:56 +0300

    commit solution

--- softuni-java-basics/T01-first-steps-in-coding/src/P09eFishTank.java
+++ softuni-java-basics/T01-first-steps-in-coding/src/P09eFishTank.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class P09eFishTank {
+import java.util.Scanner;
+
+public class P09eFishTank {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int length = Integer.parseInt(scanner.nextLine());
+        int width = Integer.parseInt(scanner.nextLine());
+        int height = Integer.parseInt(scanner.nextLine());
+        double percentMiscellaneous = Double.parseDouble(scanner.nextLine());
+        double totalVolume = (length / 10.0) * (width / 10.0) * (height / 10.0);
+        double waterVolume = totalVolume - totalVolume * percentMiscellaneous / 100;
+
+        System.out.println(waterVolume);
+    }
 }
