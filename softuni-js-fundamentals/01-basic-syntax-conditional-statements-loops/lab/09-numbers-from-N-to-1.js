3a5b55b221263d969cc1655a228b6fcf4690ef38
Miroslav Krastanov <miroslav.a.krastanov@gmail.com>
2022-09-16 15:36:54 +0300

    commit solutions

---
+++ softuni-js-fundamentals/01-basic-syntax-conditional-statements-loops/lab/09-numbers-from-N-to-1.js
@@ -0,0 +1,8 @@
+function numbersFromNTo1(input) {
+    for (let num = Number(input); num >= 1; num--) {
+        console.log(num);
+    }
+
+}
+
+numbersFromNTo1(5);
---
+++ softuni-js-fundamentals/01-basic-syntax-conditional-statements-loops/lab/10-numbers-from-M-to-N.js
@@ -0,0 +1,8 @@
+function sumOfOddNumbers(m, n) {
+    for (let num = m; num >= n; num--) {
+        console.log(num);
+    }
+
+}
+
+sumOfOddNumbers(6, 2);
