e82ec9f1b062d2d998a8eb886467402fe2738184
Miroslav Krastanov <miroslav.a.krastanov@gmail.com>
2022-09-16 12:47:50 +0300

    commit solution

--- softuni-java-basics/T01-first-steps-in-coding/src/P05eSuppliesForSchool.java
+++ softuni-java-basics/T01-first-steps-in-coding/src/P05eSuppliesForSchool.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class P05eSuppliesForSchool {
+import java.util.Scanner;
+
+public class P05eSuppliesForSchool {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int penPacks = Integer.parseInt(scanner.nextLine());
+        int sharpeyPacks = Integer.parseInt(scanner.nextLine());
+        int detergentLiters = Integer.parseInt(scanner.nextLine());
+        int discountPercent = Integer.parseInt(scanner.nextLine());
+
+        double total = penPacks * 5.80 + sharpeyPacks * 7.20 + detergentLiters * 1.20;
+        total = total - total * discountPercent / 100;
+
+        System.out.println(total);
+    }
 }
