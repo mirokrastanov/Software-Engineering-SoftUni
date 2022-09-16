ece250476396f24f5416cd92d3cd602d99f4de2d
Miroslav Krastanov <miroslav.a.krastanov@gmail.com>
2022-09-16 12:49:00 +0300

    commit solution

--- softuni-java-basics/T01-first-steps-in-coding/src/P09YardGreening.java
+++ softuni-java-basics/T01-first-steps-in-coding/src/P09YardGreening.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class P09YardGreening {
+import java.util.Scanner;
+
+public class P09YardGreening {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double sqmToDo = Double.parseDouble(scanner.nextLine());
+        double greeningCost = sqmToDo * 7.61;
+        double discount = greeningCost * 0.18;
+        double greeningTotal = greeningCost - discount;
+
+        System.out.printf("The final price is: %.2f lv.%n", greeningTotal);
+        System.out.printf("The discount is: %.2f lv.%n", discount);
+
+
+    }
 }
