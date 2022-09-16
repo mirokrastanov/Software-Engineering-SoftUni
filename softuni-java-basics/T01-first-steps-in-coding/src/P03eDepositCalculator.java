6f2ae3891c0f6182d3f73832295cd973af6a20a5
Miroslav Krastanov <miroslav.a.krastanov@gmail.com>
2022-09-16 12:47:38 +0300

    commit solution

--- softuni-java-basics/T01-first-steps-in-coding/src/P03eDepositCalculator.java
+++ softuni-java-basics/T01-first-steps-in-coding/src/P03eDepositCalculator.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class P03eDepositCalculator {
+import java.util.Scanner;
+
+public class P03eDepositCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double initialSum = Double.parseDouble(scanner.nextLine());
+        int depositDuration = Integer.parseInt(scanner.nextLine());
+        double interestRate = Double.parseDouble(scanner.nextLine());
+
+        double interestGained = interestRate * initialSum / 100;
+        double interestMonthly = interestGained / 12;
+
+        double finalSum = initialSum + depositDuration * interestMonthly;
+
+        System.out.println(finalSum);
+    }
 }
