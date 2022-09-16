8dd24988101686e41597bbd6778b8ffa2dfbda96
Miroslav Krastanov <miroslav.a.krastanov@gmail.com>
2022-09-16 12:48:45 +0300

    commit solution

--- softuni-java-basics/T01-first-steps-in-coding/src/P08eBasketballEquipment.java
+++ softuni-java-basics/T01-first-steps-in-coding/src/P08eBasketballEquipment.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class P08eBasketballEquipment {
+import java.util.Scanner;
+
+public class P08eBasketballEquipment {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int annualTax = Integer.parseInt(scanner.nextLine());
+        double sneakers = annualTax * 0.60;
+        double jersey = sneakers * 0.80;
+        double basketball = jersey / 4;
+        double miscellaneous = basketball / 5;
+        double totalCost = annualTax + sneakers + jersey + basketball + miscellaneous;
+        System.out.println(totalCost);
+    }
 }
