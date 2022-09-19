ee3ffbaabdeb97f719d04915f92bf9fbc547a4e3
Miroslav Krastanov <miroslav.a.krastanov@gmail.com>
2022-09-19 17:35:00 +0300

    new task

---
+++ softuni-js-fundamentals/01-basic-syntax-conditional-statements-loops/more-exercise/02-english-name-of-the-last-digit.js
@@ -0,0 +1,23 @@
+function engNameOfLastDigit(input) {
+    let asString = String(input);
+    let lastDigit = Number(asString.charAt(asString.length - 1));
+    let digitName = "";
+
+    switch (lastDigit) {
+        case 0: digitName = "zero"; break;
+        case 1: digitName = "one"; break;
+        case 2: digitName = "two"; break;
+        case 3: digitName = "three"; break;
+        case 4: digitName = "four"; break;
+        case 5: digitName = "five"; break;
+        case 6: digitName = "six"; break;
+        case 7: digitName = "seven"; break;
+        case 8: digitName = "eight"; break;
+        case 9: digitName = "nine"; break;
+        default: break;
+    }
+    console.log(digitName);
+
+}
+
+engNameOfLastDigit(1643);
