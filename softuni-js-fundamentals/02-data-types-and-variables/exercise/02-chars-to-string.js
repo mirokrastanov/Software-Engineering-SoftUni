aa26d134525dc4b52cf7ed63c515515d27de8a17
Miroslav Krastanov <miroslav.a.krastanov@gmail.com>
2022-09-22 15:29:13 +0300

    commit solved problem

---
+++ softuni-js-fundamentals/02-data-types-and-variables/exercise/02-chars-to-string.js
@@ -0,0 +1,11 @@
+function charToStr(param1, param2, param3) {
+    let result = param1.toString() + param2.toString() + param3.toString();
+    console.log(result);
+
+}
+
+charToStr(
+    '%',
+    '2',
+    'o'
+);
