aa3a706bf4aeb9ffd308b7a9df559d25e7c285e2
Miroslav Krastanov <miroslav.a.krastanov@gmail.com>
2022-09-22 15:28:59 +0300

    commit lab code

---
+++ softuni-js-fundamentals/02-data-types-and-variables/lab/replace-char-inside-string.js
@@ -0,0 +1,7 @@
+function solve(str, symbol, result) {
+    let res = str.replace('_', symbol);
+    let output = res ===
+        result ? "Matched" : "Not Matched";
+    console.log(output);
+}
+solve('Str_ng', 'I', 'Strong')
