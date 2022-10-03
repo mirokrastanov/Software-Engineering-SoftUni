a62bbaeb2442188c5dc405eb18a440c793cffea5
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2022-10-03 19:39:07 +0300

    lab js example

---
+++ softuni-js-fundamentals/03-arrays/lab/solve.js
@@ -0,0 +1,17 @@
+function solve() {
+
+    let student1 = "Pesho";
+    let student2 = "Gosho";
+    let student3 = "Stamat";
+    let student4 = "Mariyka";
+
+    let arrayStudents = [student1, student2, student3, student4];
+    console.log(arrayStudents);
+    arrayStudents[3] = "MARIA";
+    console.log(arrayStudents);
+    console.log(arrayStudents.length);
+
+
+}
+
+solve();
