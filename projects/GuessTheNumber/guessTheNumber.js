b09b39295b92244ff71e08c51705e1f97d8d7805
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2022-10-07 12:35:38 +0300

    add input console

---
+++ projects/GuessTheNumber/guessTheNumber.js
@@ -0,0 +1,17 @@
+function guessTheNumber() {
+
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    let computerGuess = Math.floor(Math.random() * 100);
+    
+
+
+
+
+}
+
+
+guessTheNumber();
