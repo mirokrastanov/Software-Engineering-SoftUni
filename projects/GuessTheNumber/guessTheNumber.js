3263553bdc67c9da19ff7c4b5ef35fa312356d6a
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2022-10-07 13:26:09 +0300

    Add prompt

--- projects/GuessTheNumber/guessTheNumber.js
+++ projects/GuessTheNumber/guessTheNumber.js
@@ -5,10 +5,19 @@ function guessTheNumber() {
         output: process.stdout
     });
 
-    let computerGuess = Math.floor(Math.random() * 100);
-    
+    let computerGuess = Math.floor((Math.random() * 100) + 1);
+
+    let recursiveAsyncReadLine = function () {
+        readline.question('Guess the number (0-100): ', guess => {
+            console.log(guess);
+            readline.close();
+            // TODO - if statement + sequential prompts --> emitter + listener 
+        });
+    }
+    recursiveAsyncReadLine();
 
 
+    // TODO - refine & reform
 
 
 }
