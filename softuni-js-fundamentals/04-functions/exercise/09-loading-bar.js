36e7f77dba827a05ac8df6775cbf3527b00ae31e
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2022-10-12 19:42:20 +0300

    exercise solution

---
+++ softuni-js-fundamentals/04-functions/exercise/09-loading-bar.js
@@ -0,0 +1,47 @@
+function loadingBar(inputNum) {
+    let createBar = function (inputNum) {
+        let output = [];
+        for (let i = 1; i <= 10; i++) {
+            output.push('.');
+        }
+        return output;
+    };
+    let fillBar = function (inputArr) {
+        let outputArr = inputArr;
+        for (let i = 0; i < inputNum / 10; i++) {
+            outputArr[i] = '%';
+        }
+        return outputArr;
+    };
+    let addBorders = function (inputArr) {
+        let finalArr = [];
+        for (let i = inputArr.length; i >= 0; i--) {
+            if (i > 0) {
+                finalArr[i] = inputArr[i - 1];
+            } else {
+                finalArr[i] = '[';
+            }
+        }
+        finalArr.push(']');
+        return finalArr;
+    };
+
+    let barCreated = createBar(inputNum);
+    let barFilled = fillBar(barCreated);
+    let barWithBorders = addBorders(barFilled);
+    let result = barWithBorders.join("");
+    
+    
+    if (inputNum != 100) {
+        console.log(`${inputNum}% ${result}`);
+        console.log('Still loading...');
+    } else {
+        console.log('100% Complete!');
+        console.log(result);
+    }
+
+};
+
+loadingBar(30);
+loadingBar(50);
+loadingBar(100);
