b8ab281d615910ea0114fff6ef10579745c9d298
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2022-10-19 19:14:59 +0300

    js exercise solution (optimized logic)

---
+++ softuni-js-fundamentals/05-arrays-advanced/exercise/07-search-for-a-number.js
@@ -0,0 +1,20 @@
+function searchForANumber(main, off) {
+    let toTake = off[0];
+    let toDel = off[1];
+    let target = off[2];
+    let occurences = 0;
+
+    let result = main.slice().splice(0, toTake);
+    result.splice(0, toDel);
+    
+    for (let i = 0; i < result.length; i++) {
+        if (result[i] == target) {
+            occurences++;
+        }
+    }
+    console.log(`Number ${target} occurs ${occurences} times.`);
+
+}
+
+searchForANumber([5, 2, 3, 4, 1, 6], [5, 2, 3]);
+searchForANumber([7, 1, 5, 8, 2, 7], [3, 1, 5]);
