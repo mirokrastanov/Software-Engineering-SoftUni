d58b76dea4306d15911cb47488230301e6d78ccd
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-01-16 15:35:28 +0200

    js project update

--- projects/GuessTheNumber/guessTheNumber.js
+++ projects/GuessTheNumber/guessTheNumber.js
@@ -1,15 +1,20 @@
 function guessTheNumber() {
+
     
     const readline = require('readline').createInterface({
         input: process.stdin,
         output: process.stdout
     });
+
     
     let computerGuess = Math.floor((Math.random() * 100) + 1);
+
     
     let callNewGuess = function () {};
 
     callNewGuess();
+
+
 
 }
 
