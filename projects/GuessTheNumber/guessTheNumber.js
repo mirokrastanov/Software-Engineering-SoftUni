f78d0902457801a9a605ed759db84489469844d7
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-01-16 15:35:53 +0200

    js project update

--- projects/GuessTheNumber/guessTheNumber.js
+++ projects/GuessTheNumber/guessTheNumber.js
@@ -10,7 +10,23 @@ function guessTheNumber() {
     let computerGuess = Math.floor((Math.random() * 100) + 1);
 
     
-    let callNewGuess = function () {};
+    let callNewGuess = function () {
+        readline.question('Guess the number (0-100): ', (userInput) => {
+            if (userInput.trim() != Number(userInput.trim())) {
+                
+                callNewGuess();
+            } else if (userInput.trim() == computerGuess) {
+                
+                readline.close();
+            } else if (userInput.trim() > computerGuess) {
+                
+                callNewGuess();
+            } else if (userInput.trim() < computerGuess) {
+               
+                callNewGuess();
+            }
+        });
+    };
 
     callNewGuess();
 
