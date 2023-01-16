1312c2616b40ed6365208284967965afbf4a27aa
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-01-16 15:35:02 +0200

    js project update

--- projects/GuessTheNumber/guessTheNumber.js
+++ projects/GuessTheNumber/guessTheNumber.js
@@ -1,26 +1,16 @@
 function guessTheNumber() {
-
+    
     const readline = require('readline').createInterface({
         input: process.stdin,
         output: process.stdout
     });
-
+    
     let computerGuess = Math.floor((Math.random() * 100) + 1);
+    
+    let callNewGuess = function () {};
 
-    let recursiveAsyncReadLine = function () {
-        readline.question('Guess the number (0-100): ', guess => {
-            console.log(guess);
-            readline.close();
-            // TODO - if statement + sequential prompts --> emitter + listener 
-        });
-    }
-    recursiveAsyncReadLine();
-
-
-    // TODO - refine & reform
-
+    callNewGuess();
 
 }
-
 
 guessTheNumber();
