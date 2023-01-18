d0f206130f943ecb4645ff9073227d5f63696811
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-01-18 16:10:51 +0200

    MySQL code

---
+++ softuni-js-fundamentals/10-database-basics/mysql-code/more-exercise/02-find-select-order-multiple-specific-entries.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE `softuni`;
+
+USE `softuni`;
+CREATE TABLE `students` (
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+	`first_name` VARCHAR(50),
+	`last_name` VARCHAR(50),
+	`age` INT,
+	`grade` DOUBLE
+);
+
+
+USE `softuni`;
+INSERT INTO `students` (`id`, `first_name`, `last_name`, `age`, `grade`)
+VALUES
+(1, 'Guy', 'Gilbert', 15, 4.5),
+(2, 'Kevin', 'Brown', 17, 5.4),
+(3, 'Roberto', 'Tamburello', 19, 6),
+(4, 'Linda', 'Smith', 18, 5),
+(5, 'John', 'Stones', 16, 4.25),
+(6, 'Nicole', 'Nelson', 17, 5.5);
+
+
+USE `softuni`;
+SELECT * FROM `students`
+WHERE `id` IN (2, 4);
