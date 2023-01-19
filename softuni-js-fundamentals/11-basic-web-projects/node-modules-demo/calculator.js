071187d50ccc09cd6452597379047840ed4d4530
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-01-19 14:00:19 +0200

    node modules

---
+++ softuni-js-fundamentals/11-basic-web-projects/node-modules-demo/calculator.js
@@ -0,0 +1,10 @@
+function calculate(a, b) {
+    return {
+        sum: a + b,
+        multiply: a * b,
+    }
+}
+
+module.exports = calculate;
+// export default calculate;
+// alternative way to export
