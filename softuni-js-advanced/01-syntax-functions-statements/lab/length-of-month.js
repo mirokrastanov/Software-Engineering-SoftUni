c6682a8e606de67b94a1f1d33a96022bfe1b6c3e
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-01-20 17:17:25 +0200

    js exercise

---
+++ softuni-js-advanced/01-syntax-functions-statements/lab/length-of-month.js
@@ -0,0 +1,25 @@
+const now = new Date();
+
+
+// console.log(now);
+// console.log(now.getDate());
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getMonth() + 1);
+
+
+// now.setDate(9);
+// console.log(now);
+
+// now.setMonth(-1);
+// console.log(now);
+
+
+
+// get length of month
+console.log(now);
+now.setMonth(now.getMonth() + 1);
+console.log(now);
+now.setDate(0);
+console.log(now);
+console.log(now.getDate());
