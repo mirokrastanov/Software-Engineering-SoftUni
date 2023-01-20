ba1ab6451d4ce5f3394d2a7c72cbc5dc166ab85d
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-01-20 17:17:28 +0200

    js exercise

---
+++ softuni-js-advanced/01-syntax-functions-statements/lab/params.js
@@ -0,0 +1,12 @@
+function solve(...params) {
+
+    console.log(params);
+    console.log(params[0]);
+    console.log(params[1]);
+    console.log(params[2]);
+
+    // 
+
+}
+
+solve('orange', 5, 1.95);
