e6e105464616fb8fcc8c1d66615bc391dc0d70e7
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-01-20 18:01:23 +0200

    js exercise

---
+++ softuni-js-advanced/02-arrays-nested-arrays/more-exercise/hackerrank-car-obj-1.js
@@ -0,0 +1,22 @@
+function printObjectProperty(myObject) {
+    console.log(myObject);
+  
+  }
+  
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  _input = "";
+  process.stdin.on("data", function (input) {
+      _input += input;
+  });
+  
+  process.stdin.on("end", function () {
+      var obj = _input.split(' ');
+      var myObject = new Object;
+    
+      myObject.type = obj[0];
+      myObject.model = obj[1];
+      myObject.color = obj[2];
+  
+      printObjectProperty(myObject);
+  });
