ecec0a667f5b206e07142b4efd66f19b4a2d8504
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-01-21 12:07:42 +0200

    js exercise

---
+++ softuni-js-advanced/01-syntax-functions-statements/more-exercise/multi-sort.js
@@ -0,0 +1,25 @@
+function sortLibrary() {
+
+    library = library.sort((a,b) => a['title'].localeCompare(b['title']));
+    console.log(library);
+}
+
+var library = [
+    {
+        author: 'Bill Gates',
+        title: 'The Road Ahead',
+        libraryID: 1254
+    },
+    {
+        author: 'Steve Jobs',
+        title: 'Walter Isaacson',
+        libraryID: 4264
+    },
+    {
+        author: 'Suzanne Collins',
+        title: 'Mockingjay: The Final Book of The Hunger Games',
+        libraryID: 3245
+    }
+];
+
+sortLibrary();
