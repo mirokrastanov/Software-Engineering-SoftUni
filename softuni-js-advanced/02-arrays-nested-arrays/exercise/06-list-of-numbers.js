ff2aa4fa82350b7f06f5f31c67269370008627dd
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-01-23 17:21:42 +0200

    js exercise

---
+++ softuni-js-advanced/02-arrays-nested-arrays/exercise/06-list-of-numbers.js
@@ -0,0 +1,11 @@
+function list(input) {
+    let sorted = input.sort((a,b) => a.localeCompare(b));
+    let counter = 0;
+    sorted.forEach(element => {
+        counter++;
+        console.log(`${counter}.${element}`);
+    });
+
+}
+
+list(["John", "Bob", "Christina", "Ema"]);
