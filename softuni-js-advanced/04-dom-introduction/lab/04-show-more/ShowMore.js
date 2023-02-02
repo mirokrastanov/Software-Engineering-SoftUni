52d1bc3c10cac80440f434adec5e81af4d06b978
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-02-02 21:23:01 +0200

    js dom exercise

---
+++ softuni-js-advanced/04-dom-introduction/lab/04-show-more/ShowMore.js
@@ -0,0 +1,8 @@
+function showText() {
+    const extraElement = document.querySelector("#text");
+    const btnElement = document.querySelector("#more");
+
+    extraElement.style.display = "inline";
+    btnElement.style.display = "none";
+
+}
