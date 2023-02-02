79fb96e70e86abbc3a8abd68de4d42b782d8b550
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-02-02 21:22:18 +0200

    js dom exercise

--- softuni-js-advanced/04-dom-introduction/exercise/05-table-search-engine/solution.js
+++ softuni-js-advanced/04-dom-introduction/exercise/05-table-search-engine/solution.js
@@ -2,7 +2,34 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const table = document.querySelector("tbody");
+      const rows = Array.from(table.querySelectorAll("tr"));
+      let searchText = document.querySelector("#searchField").value;
+      searchText = searchText
+         .trim()
+         .split(" ")
+         .filter(a => a != "");
 
+      for (let i = 0; i < rows.length; i++) {
+         let row = rows[i];
+         let cells = row.children;
+         for (let j = 0; j < cells.length; j++) {
+            let rowMatched = false;
+            let current = cells[j];
+            let content = current.textContent;
+            for (let p = 0; p < searchText.length; p++) {
+               if (content.includes(searchText[p])) {
+                  row.classList.add("select");
+                  rowMatched = true;
+                  break;
+               } else {
+                  row.classList.remove("select");
+               }
+            }
+            if (rowMatched) {
+               break;
+            }
+         }
+      }
    }
 }
