ce6f712b06c0f737382a6b75cd65fe4be2dbc2ba
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-02-03 22:53:06 +0200

    js test

---
+++ softuni-js-advanced/04-dom-introduction/lab/test-parse.js
@@ -0,0 +1,8 @@
+function solve(input) {
+
+
+console.log(JSON.parse(input));
+console.log(input);
+}
+
+solve('["PizzaHut - Peter 500, George 300, Mark 800", "TheLake - Bob 1300, Joe 780, Jane 660"]');
