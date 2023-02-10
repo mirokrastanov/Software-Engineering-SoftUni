81be9fbdb7826910bc5197c0a372d0f5313820c1
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-02-10 17:06:04 +0200

    js exercise

---
+++ softuni-js-advanced/06-advanced-functions/lab/test-f-sum.js
@@ -0,0 +1,33 @@
+function add(num) {
+    let sum = 0;
+
+    if (!num) {
+        return;
+    } else {
+        function inner(next) {
+            sum += next;
+            return inner;
+        }
+    }
+
+    inner.toString = () => {
+        return sum;
+    };
+
+    // function inner(next) {
+    //     sum += next;
+    //     return inner;
+    // }
+    // inner.toString = () => {
+    //     return sum;
+    // }
+
+    return inner(num);
+}
+
+// console.log(add(1)); // 1
+// console.log(add(1)(6)(-3)); // 4
+
+console.log(add(1).toString()); // 1
+console.log(add(1)(6)(-3).toString()); // 4
+
