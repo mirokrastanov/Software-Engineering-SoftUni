310da19ff1294b5f5f4ffa6e11fc02c4708f61a0
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-02-13 23:55:00 +0200

    js exercise

--- softuni-js-advanced/06-advanced-functions/exercise/06-monkey-patcher.js
+++ softuni-js-advanced/06-advanced-functions/exercise/06-monkey-patcher.js
@@ -36,7 +36,7 @@ function solution(toAdd) {
             let upMajority = (reportedUpvotes / totalVotes * 100) > 66;
             let downMajority = (reportedDownvotes / totalVotes * 100) > 66;
             // ako dava gre6ki - da probvam totalVotes < 10 da go metna tyka kato opravat judge
-            if (upMajority) {
+            if (upMajority && totalVotes >= 10) {
                 reportedRating = 'hot';
             } else if (!upMajority && !downMajority && reportedScore >= 0 && totalVotes > 100) {
                 reportedRating = 'controversial';
@@ -45,12 +45,13 @@ function solution(toAdd) {
             } else if (this.upvotes + this.downvotes < 10) {
                 reportedRating = 'new';
             }
-            console.log([reportedUpvotes, reportedDownvotes, reportedScore, reportedRating]);
+            // console.log([reportedUpvotes, reportedDownvotes, reportedScore, reportedRating]);
+            return [reportedUpvotes, reportedDownvotes, reportedScore, reportedRating];
             // report + obfuscation rlz verified = exp arr
             break;
     }
 }
-
+/*
 let post = {
     id: '3',
     author: 'emil',
@@ -65,5 +66,27 @@ let score = solution.call(post, 'score'); // [127, 127, 0, 'controversial']
 for (let i = 0; i < 50; i++) {
     solution.call(post, 'downvote');
 }
-
 score = solution.call(post, 'score');     // [139, 189, -50, 'unpopular']
+*/
+
+
+const { expect } = require('chai');
+var forumPost = {
+    id: '1',
+    author: 'pesho',
+    content: 'hi guys',
+    upvotes: 0,
+    downvotes: 0
+};
+solution.call(forumPost, 'upvote');
+var answer = solution.call(forumPost, 'score');
+var expected = [1, 0, 1, 'new'];
+console.log(answer);
+
+compareScore(expected, answer);
+function compareScore(expected, answer) {
+    expect(expected[0]).to.equal(answer[0], 'Incorrect number of upvotes');
+    expect(expected[1]).to.equal(answer[1], 'Incorrect number of downvotes');
+    expect(expected[2]).to.equal(answer[2], 'Incorrect score');
+    expect(expected[3]).to.equal(answer[3], 'Incorrect rating');
+}
