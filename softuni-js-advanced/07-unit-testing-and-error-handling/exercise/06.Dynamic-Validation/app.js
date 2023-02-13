39bfef2cab78d42e0c0341d92a0bf4a8d8a1a582
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-02-13 23:25:18 +0200

    js exercise

--- softuni-js-advanced/07-unit-testing-and-error-handling/exercise/06.Dynamic-Validation/app.js
+++ softuni-js-advanced/07-unit-testing-and-error-handling/exercise/06.Dynamic-Validation/app.js
@@ -1,3 +1,15 @@
 function validate() {
-    // TODO
+    
+    let input = document.querySelector('#email');
+    let regex = new RegExp(/[a-z]+@[a-z]+\.[a-z]+/, 'g');
+    
+    input.addEventListener('change', (e) => {
+        let matches = (input.value).match(regex);
+        if (matches && matches[0] == input.value) {
+            input.classList.remove('error');
+        } else {
+            input.classList.add('error');
+        }
+    })
+
 }
