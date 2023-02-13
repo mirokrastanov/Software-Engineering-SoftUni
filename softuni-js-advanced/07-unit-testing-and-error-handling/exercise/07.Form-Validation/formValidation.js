183390fb8f2bdbf9dfcf34abb4e480fd0c967fb1
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-02-14 00:40:16 +0200

    js exercise

--- softuni-js-advanced/07-unit-testing-and-error-handling/exercise/07.Form-Validation/formValidation.js
+++ softuni-js-advanced/07-unit-testing-and-error-handling/exercise/07.Form-Validation/formValidation.js
@@ -1,78 +1,65 @@
 function validate() {
     let btn = document.querySelector('#submit');
-    let username = document.querySelector('#username');
+    btn.type = 'button';
+    let userEl = document.querySelector('#username');
     let emailEl = document.querySelector('#email');
     let passEl = document.querySelector('#password');
     let confEl = document.querySelector('#confirm-password');
     let company = document.querySelector('#company');
-    let usernamePattern = new RegExp(/[^a-zA-Z0-9]/, 'g');
-    let passwordPattern = new RegExp(/[^\_a-zA-Z0-9]/, 'g');
-    let emailPattern = new RegExp(/[@]+[\w\s\d\W\S\D]*[.]+[\w\s\d\W\S\D]*/, 'g');
     let companyInfo = document.querySelector('#companyInfo');
     let valid = document.querySelector('#valid');
     let compNumber = document.querySelector('#companyNumber');
-    let [userValid, passValid, emailValid] = [true, true, true];
-    let tempEmail;
+    let usernamePattern = new RegExp(/[^a-zA-Z0-9]/, 'g');
+    let passwordPattern = new RegExp(/[^\_a-zA-Z0-9]/, 'g');
+    let emailPattern = new RegExp(/[@]+[\w\s\d\W\S\D]*[.]+[\w\s\d\W\S\D]*/, 'g');
+
+    company.addEventListener('change', (e) => {
+        console.log(e.target.checked);
+        if (company.checked) companyInfo.style.display = 'block';
+        else companyInfo.style.display = 'none';
+    });
 
     btn.addEventListener('click', (e) => {
-        e.preventDefault();
-        if (username.value.length < 3 || username.value.length > 20 || usernamePattern.test(username.value)) {
+        // e.preventDefault();
+        let isValid = []
+        let [password, confPass, email, username] = [passEl.value, confEl.value, emailEl.value, userEl.value];
+        if (username.length < 3 || username.length > 20 || usernamePattern.test(username)) {
             username.style.borderColor = 'red';
-            userValid = false;
+            isValid.push(false);
         } else {
-            username.style.border = 'none';
-            userValid = true;
-        } 
-        let password = passEl.value;
-        let confPass = confEl.value;
-        // console.log(password, confPass, password != confPass, passwordPattern.test(password));
-        if (password.length < 5 || password.length > 15 || passwordPattern.test(password) || password != confPass) {
+            userEl.style.border = 'none';
+            isValid.push(true);
+        }
+        if (password.length < 5 || password.length > 15 ||
+            passwordPattern.test(password) || password != confPass) {
             passEl.style.borderColor = 'red';
             confEl.style.borderColor = 'red';
-            passValid = false;
+            isValid.push(false);
         } else {
             passEl.style.border = 'none';
             confEl.style.border = 'none';
-            passValid = true;
+            isValid.push(true);
         }
-        let email = emailEl.value;
         if (!emailPattern.test(email)) {
-            console.log('vutre sum');
             emailEl.style.borderColor = 'red';
-            emailValid = false;
+            isValid.push(false);
         } else {
             emailEl.style.border = 'none';
-            emailValid = true;
-            tempEmail = email;
+            isValid.push(true);
         }
-        console.log(userValid, passValid, emailValid, email);
-        if (userValid && passValid && emailValid) {
-            if (company.checked) {
-                companyInfo.style.display = 'block';
-                if (compNumber.value < 1000 && compNumber.value > 9999) {
-                    compNumber.style.borderColor = 'red';
-                    valid.style.display = 'none';
-                } else {
-                    compNumber.style.border = 'none';
-                    valid.style.display = 'block';
-                }
+        if (company.checked) {
+            if (compNumber.value < 1000 || compNumber.valid > 9999) {
+                compNumber.style.borderColor = 'red';
+                isValid.push(false);
             } else {
-                companyInfo.style.display = 'none';
-                valid.style.display = 'block';
+                compNumber.style.border = 'none';
+                isValid.push(true);
             }
-        } else {
+        }
+        if (isValid.includes(false)) {
             valid.style.display = 'none';
-        }
-
-    });
-
-    compNumber.addEventListener('change', () => {
-        if (compNumber.value < 1000 && compNumber.valid > 9999) {
-            compNumber.style.borderColor = 'red';
         } else {
-            compNumber.style.border = 'none';
+            valid.style.display = 'block';
         }
     });
-
 }
-module.exports = validate;
