a684e60328487c3aefb9e0ca937aaff66e8a28b0
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-02-16 01:21:10 +0200

    js exercise

---
+++ softuni-js-advanced/09-prototypes-and-inheritance/exercise/06-posts.js
@@ -0,0 +1,73 @@
+function solution() {
+    class Post {
+        constructor(title, content) {
+            this.title = title;
+            this.content = content;
+        }
+        toString() {
+            return `Post: ${this.title}\nContent: ${this.content}`;
+        }
+    }
+    class SocialMediaPost extends Post {
+        constructor(title, content, likes, dislikes) {
+            super(title, content);
+            this.likes = Number(likes);
+            this.dislikes = Number(dislikes);
+            this.comments = [];
+        }
+        addComment(comment) {
+            this.comments.push(comment);
+        }
+        toString() {
+            let output = super.toString() + `\nRating: ${this.likes - this.dislikes}`;
+            if (this.comments.length == 0) return output;
+            output += `\nComments:`;
+            this.comments.forEach(comment => {
+                output += `\n * ${comment}`;
+            });
+            return output;
+        }
+    }
+    class BlogPost extends Post {
+        constructor(title, content, views) {
+            super(title, content);
+            this.views = Number(views);
+        }
+        view() {
+            this.views++;
+            // console.log(this); // returns the object
+            return this;
+        }
+        toString() {
+            return super.toString() + `\nViews: ${this.views}`;
+        }
+    }
+    return { Post, SocialMediaPost, BlogPost }
+}
+const classes = solution();
+let post = new classes.Post("Post", "Content");
+
+console.log(post.toString());
+
+// Post: Post
+// Content: Content
+
+let scm = new classes.SocialMediaPost("TestTitle", "TestContent", 25, 30);
+
+scm.addComment("Good post");
+scm.addComment("Very good post");
+scm.addComment("Wow!");
+
+console.log(scm.toString());
+
+// Post: TestTitle
+// Content: TestContent
+// Rating: -5
+// Comments:
+//  * Good post
+//  * Very good post
+//  * Wow!
+let bgp = new classes.BlogPost('Blog-test', 'some content', 55);
+console.log(bgp.toString());
+bgp.view().view().view();
+console.log(bgp.toString());
