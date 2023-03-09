d67dd9d34075a3457e275c1bc2c7a171be23673a
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-03-09 22:52:41 +0200

    js exercise

--- softuni-js-applications/03-remote-data-and-authentication/exercise/05.Fisher-Game/src/register.js
+++ softuni-js-applications/03-remote-data-and-authentication/exercise/05.Fisher-Game/src/register.js
@@ -1,1 +1,53 @@
-console.log('TODO:// Implement Register functionality');
+function main() {
+    const logOutBtn = document.querySelector('#user');
+    const homeBtn = document.querySelector('#home');
+    const logInBtn = document.querySelector('#login');
+    const regBtn = document.querySelector('#register');
+    logOutBtn.style.display = 'none';
+    regBtn.classList.add('active');
+    logOutBtn.classList.remove('active');
+    homeBtn.classList.remove('active');
+    logInBtn.classList.remove('active');
+    
+    const formElement = document.querySelector('form#register');
+    const url = 'http://localhost:3030/users/register';
+
+    formElement.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        let formData = new FormData(e.currentTarget);
+        let { email, password, rePass } = Object.fromEntries(formData);
+        try {
+            if ([...formData.values()].some(x => x == '')) {
+                throw new Error('Invalid inputs');
+            } else if (password != rePass) {
+                throw new Error('The passwords do not match');
+            }
+            let res = await fetch(url, {
+                method: 'POST',
+                headers: { 'Content-type': 'application/json' },
+                body: JSON.stringify({ email, password, rePass }),
+            });
+            if (!res.ok) {
+                let error = await res.json();
+                throw new Error(error.message);
+            }
+            let data = await res.json();
+            console.log(data);
+            let user = {
+                email: data.email,
+                id: data._id,
+                token: data.accessToken,
+            };
+            localStorage.setItem('userData', JSON.stringify(user));
+            window.location = './index.html';
+        } catch (error) {
+            console.log(error.message);
+            formElement.reset();
+        }
+    });
+
+
+
+}
+
+main();
