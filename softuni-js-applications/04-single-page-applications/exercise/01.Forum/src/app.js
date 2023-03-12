d89455c7dcb22896e78bebbddbc8828f64abf4aa
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-03-12 12:50:38 +0200

    implemented GET & POST to the server

--- softuni-js-applications/04-single-page-applications/exercise/01.Forum/src/app.js
+++ softuni-js-applications/04-single-page-applications/exercise/01.Forum/src/app.js
@@ -1,11 +1,7 @@
+import { elements } from "./elements.js";
 import { loadHome } from "./home.js";
 import { clearTopic, postTopic } from "./topic.js";
 
-export const elements = {
-    homeBtn: document.querySelector('#home-btn'),
-    cancelBtn: document.querySelector('.cancel'),
-    postBtn: document.querySelector('.public'),
-};
 elements.homeBtn.addEventListener('click', async (e) => loadHome(e));
 elements.cancelBtn.addEventListener('click', async (e) => clearTopic(e));
 elements.postBtn.addEventListener('click', async (e) => postTopic(e));
