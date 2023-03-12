a8de86ba2c926d28f91202e7431a465738c01ff6
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-03-12 12:50:42 +0200

    implemented GET & POST to the server

--- softuni-js-applications/04-single-page-applications/exercise/01.Forum/src/home.js
+++ softuni-js-applications/04-single-page-applications/exercise/01.Forum/src/home.js
@@ -1,7 +1,9 @@
-
+import { elements } from "./elements.js";
+import { loadTopics } from "./topic.js";
 
 export function loadHome(e) {
     e.preventDefault();
     document.querySelector('.new-topic-border').style.display = 'block';
+    loadTopics();
 
 }
