fd7aa2b3929728300f7409ae6cd70f97e99dba26
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-03-13 21:48:59 +0200

    implement initial structure

---
+++ softuni-js-applications/04-single-page-applications/exercise/02.Movies/src/app.js
@@ -0,0 +1,33 @@
+// [] adjust HTML
+// [] app.js/router.js/util.js
+// [] view placeholders
+// [] view implementations
+// [] fetch request function --> return await res.json()
+// [] catalog
+// [] login
+// [] register
+// [] create
+// [] details
+// [] like
+// [] edit
+// [] delete
+// [] guest
+import { loadHome, hideSections } from './home.js';
+
+const elements = {
+    sections: document.querySelectorAll('.view-section'),
+    home: document.querySelector('#home-page'),
+    
+};
+const routes = {
+
+};
+
+
+// eventListener + view controls
+
+export {
+    elements,
+    routes,
+
+}
