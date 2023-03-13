925684f1013731b6731175d75bb4f9f6ca272789
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-03-13 21:49:04 +0200

    implement initial structure

---
+++ softuni-js-applications/04-single-page-applications/exercise/02.Movies/src/home.js
@@ -0,0 +1,16 @@
+import { elements, routes } from './app.js';
+
+function hideSections() {
+    [...elements.sections].forEach(x => x.style.display = 'none');
+}
+
+function loadHome() {
+    hideSections();
+    elements.home.style.display = 'block';
+}
+
+export {
+    hideSections,
+    loadHome,
+
+}
