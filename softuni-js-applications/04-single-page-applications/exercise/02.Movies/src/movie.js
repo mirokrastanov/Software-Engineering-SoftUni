0daadbe40c7bfe23918ebc2ff85a4e4ee6377344
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-03-15 18:12:11 +0200

    added dynamic like/remove like functionality

--- softuni-js-applications/04-single-page-applications/exercise/02.Movies/src/movie.js
+++ softuni-js-applications/04-single-page-applications/exercise/02.Movies/src/movie.js
@@ -15,12 +15,6 @@ async function loadMovieSection(id) {
     elements.movie.replaceChildren(movieElement);
     await updateMovieBtns();
     let btnsCtr = elements.movie.querySelector('.container .row.bg-light.text-dark .col-md-4.text-center');
-    let btns = {
-        delete: btnsCtr.querySelector('a.btn.btn-danger'),
-        edit: btnsCtr.querySelector('a.btn.btn-warning'),
-        like: btnsCtr.querySelector('a.btn.btn-primary'),
-        indicator: btnsCtr.querySelector('span.enrolled-span'),
-    };
     btnsCtr.addEventListener('click', (e) => {
         btnsHandler(e);
     });
@@ -43,6 +37,7 @@ function createMovieExample(movie) {  // TODO - add likes counter
             <a class="btn btn-danger" href="javascript:void(0)">Delete</a>
             <a class="btn btn-warning" href="javascript:void(0)">Edit</a>
             <a class="btn btn-primary" href="javascript:void(0)">Like</a>
+            <a class="btn btn-del-like" href="javascript:void(0)">Remove Like</a>
             <span class="enrolled-span">Liked ###</span>
         </div>
     </div>`;
@@ -62,20 +57,19 @@ async function updateMovieBtns() {
         delete: btnsCtr.querySelector('a.btn.btn-danger'),
         edit: btnsCtr.querySelector('a.btn.btn-warning'),
         like: btnsCtr.querySelector('a.btn.btn-primary'),
+        dislike: btnsCtr.querySelector('a.btn.btn-del-like'),
         indicator: btnsCtr.querySelector('span.enrolled-span'),
     };
-    let movieLikes = await getLikes(movieData.id);
-    btns.indicator.textContent = `Liked ${movieLikes}`;
-    // and use it below for the 2 conditions - logged in / not
-    btns.indicator.style.display = 'inline-block';
+    updateMovieLikes(movieData.id);
     if (!isLogged()) {  // NOT logged in
         btns.delete.style.display = 'none';
         btns.edit.style.display = 'none';
         btns.like.style.display = 'none';
-        // update indicator's textContent with # likes
+        btns.dislike.style.display = 'none';
     } else {  // logged in
         if (movieData.ownerId == userData._id) { // Owner: edit/delete
             btns.like.style.display = 'none';
+            btns.dislike.style.display = 'none';
             btns.delete.style.display = 'inline-block';
             btns.edit.style.display = 'inline-block';
         } else { // NOT Owner: like
@@ -83,24 +77,50 @@ async function updateMovieBtns() {
             btns.edit.style.display = 'none';
             if (await hasCurrentUserLikedThisMovie(userData._id, movieData.id)) {
                 btns.like.style.display = 'none'; // hide like btn
+                btns.dislike.style.display = 'inline-block';
             } else {
                 btns.like.style.display = 'inline-block'; // show it
+                btns.dislike.style.display = 'none';
             }
         }
     }
-
 }
 
-function btnsHandler(e) {
+async function updateMovieLikes(movieId) {
+    let btnsCtr = elements.movie.querySelector('.container .row.bg-light.text-dark .col-md-4.text-center');
+    let btns = {
+        delete: btnsCtr.querySelector('a.btn.btn-danger'),
+        edit: btnsCtr.querySelector('a.btn.btn-warning'),
+        like: btnsCtr.querySelector('a.btn.btn-primary'),
+        dislike: btnsCtr.querySelector('a.btn.btn-del-like'),
+        indicator: btnsCtr.querySelector('span.enrolled-span'),
+    };
+    let movieLikes = await getLikes(movieId);
+    btns.indicator.textContent = `Liked ${movieLikes}`;
+    btns.indicator.style.display = 'inline-block';
+}
+
+async function btnsHandler(e) {
     e.preventDefault();
+    let userData = JSON.parse(localStorage.getItem('userData'));
+    let movieData = {
+        html: elements.movie.querySelector('.container'),
+        ownerId: elements.movie.querySelector('.container').dataset.ownerId,
+        id: elements.movie.querySelector('.container').dataset.id,
+    };
     if (e.target.tagName == 'A') {
         if (e.target.textContent == 'Delete') {
             // function for movie deletion
         } else if (e.target.textContent == 'Edit') {
             // function for movie edit - from the edit-movie.js
         } else if (e.target.textContent == 'Like') {
-            // function for liking + check for just 1 like
-            // after a successful like - updateMovieBtns();
+            let likeData = await addLike(userData, movieData.id);
+            updateMovieBtns();
+        } else if (e.target.textContent == 'Remove Like') {
+            let likeData = await getCurrentUserLikeforThisMovie(userData._id, movieData.id);
+            let likeId = likeData[0]._id;
+            await revokeLike(likeId);
+            updateMovieBtns();
         }
     }
 }
@@ -140,6 +160,47 @@ async function hasCurrentUserLikedThisMovie(userId, movieId) {
     }
 }
 
+async function getCurrentUserLikeforThisMovie(userId, movieId) {
+    let url = elements.baseURL + `/data/likes?where=movieId%3D%22${movieId}%22%20and%20_ownerId%3D%22${userId}%22`;
+    let options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+    let res = await request(url, options);
+    return await res;
+}
+
+async function addLike(userData, movieId) {
+    let url = elements.baseURL + '/data/likes';
+    let options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Authorization': userData.accessToken,
+        },
+        body: JSON.stringify({
+            movieId,
+        }),
+    };
+    let res = await request(url, options);
+    let data = await res;
+    return data;
+}
+
+async function revokeLike(likeId) {
+    let userData = JSON.parse(localStorage.getItem('userData'));
+    let url = elements.baseURL + `/data/likes/${likeId}`;
+    let options = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Authorization': userData.accessToken,
+        },
+    };
+    let res = await request(url, options);
+}
 
 export {
     loadMovieSection,
