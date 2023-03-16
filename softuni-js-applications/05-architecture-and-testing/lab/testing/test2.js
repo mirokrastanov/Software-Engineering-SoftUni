666e767d4e3f35f8e4bf1cd4226483375c2b0b18
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-03-16 14:08:14 +0200

    js tests

--- softuni-js-applications/05-architecture-and-testing/lab/testing/test2.js
+++ softuni-js-applications/05-architecture-and-testing/lab/testing/test2.js
@@ -11,9 +11,22 @@ describe('E2E tests', async function () {
     beforeEach(async () => { page = await browser.newPage(); }); // before Each test - open a new tab
     afterEach(async () => { await page.close(); });              // after Each test - close the tab
 
+    // it('works', async () => {
+    //     await page.goto('https://softuni.bg');
+    //     await page.screenshot({ path: 'test.png' });
+    //     expect(1).to.equal(1);
+    // });
+
+
     it('works', async () => {
         await page.goto('https://softuni.bg');
+        await page.click('a >> text=Семинари'); // selector by visual element - find an Anchor tag and look 
+        // for something with a text of 'Семинари' --- could be nested element, a span or p or whatever, as long
+        // as it is inside the anchor and has that text
+        // await page.click('article:has(div.promo)'); // with a regular css selector (not recommended by playwright as it could be changed/updated)
         await page.screenshot({ path: 'test.png' });
+        // await page.waitForNavigation();
+        // await page.waitForLoadState();
         expect(1).to.equal(1);
     });
 });
