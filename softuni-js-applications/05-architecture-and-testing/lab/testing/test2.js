088072853c6e68d8fff4ecc5bf7a2f056bb55e68
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-03-16 13:23:52 +0200

    initial testing with playwright, chai & mocha

--- softuni-js-applications/05-architecture-and-testing/lab/testing/test2.js
+++ softuni-js-applications/05-architecture-and-testing/lab/testing/test2.js
@@ -1,9 +1,15 @@
 const { chromium } = require('playwright-chromium');
-async function test() {
-    const browser = await chromium.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto('https://softuni.bg/');
-    await page.screenshot({ path: `softuni.png` });
-    await browser.close();
-}
-test();
+const { expect, assert } = require('chai');
+
+let browser, page; // declare reusable variables
+
+describe('E2E tests', async function () {
+    before(async () => { browser = await chromium.launch(); });  // before all tests - launch chromium browser
+    after(async () => { await browser.close(); });               // after all tests - close the browser
+    beforeEach(async () => { page = await browser.newPage(); }); // before Each test - open a new tab
+    afterEach(async () => { await page.close(); });              // after Each test - close the tab
+
+    
+
+});
+
