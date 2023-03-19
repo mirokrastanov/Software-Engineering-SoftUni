45bd4c88e3b4d9fd5a8d0dbf68c9deb1bd721936
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-03-19 12:40:11 +0200

    initialized views architecture

--- softuni-js-applications/05-architecture-and-testing/exercise/03.SoftTerest/app.js
+++ softuni-js-applications/05-architecture-and-testing/exercise/03.SoftTerest/app.js
@@ -1,7 +1,7 @@
 // MVC Model
 // app.js - controller
 // views - login, register, dashboard/catalog, home
-// router - paths for all views
+// router - paths for all views (add hrefs for /login, etc)
 // api - methods, async functions
 
 document.querySelector("#defSection").remove();
