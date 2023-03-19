a8a02cf4216b7dea0f7b4fa3e1b78e1c05302061
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-03-19 18:32:34 +0200

    implemented login

--- softuni-js-applications/05-architecture-and-testing/exercise/03.SoftTerest/app.js
+++ softuni-js-applications/05-architecture-and-testing/exercise/03.SoftTerest/app.js
@@ -3,6 +3,7 @@
 // views - login, register, dashboard/catalog, home
 // router - paths for all views (add hrefs for /login, etc)
 // api - methods, async functions
+import { logout } from "./src/api/user.js";
 import { initializer } from "./src/router.js";
 import { showCatalog } from "./src/views/catalog.js";
 import { showCreate } from "./src/views/create.js";
--- softuni-js-applications/05-architecture-and-testing/exercise/03.SoftTerest/src/views/login.js
+++ softuni-js-applications/05-architecture-and-testing/exercise/03.SoftTerest/src/views/login.js
@@ -1,5 +1,21 @@
-const section = document.querySelector('#loginView');
+import { login } from "../api/user.js";
 
+const section = document.querySelector('#loginView');
+const form = section.querySelector('form');
+form.addEventListener('submit', onSubmit);
+
+let ctx = null;
 export function showLogin(context) {
+    ctx = context;
     context.showSection(section);
+}
+
+async function onSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const { email, password } = Object.fromEntries(formData);
+    await login(email, password);
+    console.log('Login successful');
+    ctx.updateNav();
+    ctx.goTo('/catalog');
 }
