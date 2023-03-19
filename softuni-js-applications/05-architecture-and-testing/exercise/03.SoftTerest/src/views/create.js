21ba0f087b434dbac1a095f25b26611d407e1898
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-03-19 18:43:40 +0200

    implemeneted create idea functionality

--- softuni-js-applications/05-architecture-and-testing/exercise/03.SoftTerest/src/views/create.js
+++ softuni-js-applications/05-architecture-and-testing/exercise/03.SoftTerest/src/views/create.js
@@ -1,5 +1,21 @@
-const section = document.querySelector('#createView');
+import { createIdea } from "../api/data.js";
 
+const section = document.querySelector('#createView');
+const form = section.querySelector('form');
+form.addEventListener('submit', onSubmit);
+
+let ctx = null;
 export function showCreate(context) {
+    ctx = context;
     context.showSection(section);
+}
+
+async function onSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const { title, description, imageURL } = Object.fromEntries(formData);
+    await createIdea({ title, description, img: imageURL });
+    console.log('Idea created');
+    form.reset();
+    ctx.goTo('/catalog');
 }
