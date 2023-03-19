5bd96d6de928ed67654d91786e94fd7f5777040d
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-03-19 18:09:14 +0200

    implemented registration

--- softuni-js-applications/05-architecture-and-testing/exercise/03.SoftTerest/src/views/register.js
+++ softuni-js-applications/05-architecture-and-testing/exercise/03.SoftTerest/src/views/register.js
@@ -1,5 +1,26 @@
-const section = document.querySelector('#registerView');
+import { register } from "../api/user.js";
 
+const section = document.querySelector('#registerView');
+const form = document.querySelector('form');
+form.addEventListener('submit', onSubmit);
+
+let ctx = null;
 export function showRegister(context) {
+    ctx = context;
     context.showSection(section);
+}
+
+async function onSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const { email, password, repeatPassword } = Object.fromEntries(formData);
+    // TODO - add more verifiers
+    if (password != repeatPassword) {
+        console.log('Passwords do not match');
+    } else {
+        await register(email, password);
+        console.log('Registration successful');
+        ctx.updateNav();
+        ctx.goTo('/catalog');
+    }
 }
