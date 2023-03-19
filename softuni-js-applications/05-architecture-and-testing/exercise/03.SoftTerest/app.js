d8c72b769e6bf851ac8e3be926a8fb43df78bd22
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-03-19 17:21:13 +0200

    nav now functional

--- softuni-js-applications/05-architecture-and-testing/exercise/03.SoftTerest/app.js
+++ softuni-js-applications/05-architecture-and-testing/exercise/03.SoftTerest/app.js
@@ -29,8 +29,36 @@ const context = {
 
 };
 
-showHomeView(context);
-
 function showSection(section) {
     main.replaceChildren(section);
+}
+
+function onNavigate(e) {
+    e.preventDefault();
+    let target = e.target;
+    if (target.tagName == 'IMG') {
+        target = target.parentElement;
+    }
+    if (target.tagName == 'A') {
+        let url = new URL(target.href);
+        goTo(url.pathname);
+    }
+}
+
+function goTo(name, ...params) {
+    const handler = links[name];
+    if (typeof(handler) == 'function') {
+        handler(context, ...params);
+    }
+
+    function updateNav() {
+        const user = JSON.parse(sessionStorage.getItem('user'));
+        if (user) {
+            document.querySelectorAll('.user').forEach(x => x.style.display = 'block');
+            document.querySelectorAll('.guest').forEach(x => x.style.display = 'none');
+        } else {
+            document.querySelectorAll('.user').forEach(x => x.style.display = 'none');
+            document.querySelectorAll('.guest').forEach(x => x.style.display = 'block');
+        }
+    }
 }
