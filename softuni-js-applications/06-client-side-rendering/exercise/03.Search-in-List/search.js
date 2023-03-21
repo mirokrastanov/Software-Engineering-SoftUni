0f2847925c4b301e28dc9145111f9682b388b9e9
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-03-21 13:00:59 +0200

    js exercise

--- softuni-js-applications/06-client-side-rendering/exercise/03.Search-in-List/search.js
+++ softuni-js-applications/06-client-side-rendering/exercise/03.Search-in-List/search.js
@@ -1,42 +1,106 @@
-
 import { html, render } from '../node_modules/lit-html/lit-html.js';
 import { styleMap } from '../node_modules/lit-html/directives/style-map.js';
 import { towns } from './towns.js';
 
-function search() {
-   const input = document.querySelector('#searchText');
-   const resultDiv = document.querySelector('#result');
-   const searchBtn = document.querySelector('button');
-   const townsDiv = document.querySelector('#towns');
-   const ulTemplate = () => html`
+const body = document.querySelector('body');
+const articleTemplate = (towns, match) => html`
+<article>
+   <div id="towns">
       <ul>
-         ${towns.map(x => html`<li>${x}</li>`)}
+         ${towns.map(town => 
+            html`<li class=${match && town.includes(match) ? 'active' : ''}>${town}</li>`
+         )};
       </ul>
-   `;
-   update();
+   </div>
+   <input type="text" id="searchText" />
+   <button @click=${onSearch}>Search</button>
+   <div id="result"></div>
+</article>
+`;
 
-   searchBtn.addEventListener('click', onSearch);
-
-   function onSearch(e) {
-      e.preventDefault();
-      let inputValue = input.value.trim();
-      if (inputValue != '') {
-         towns.forEach((x, i) => {
-            if (x.includes(inputValue)) {
-               document.querySelector(`li:nth-of-type(${i + 1})`)
-                  .classList.add('active');
-            } else {
-               document.querySelector(`li:nth-of-type(${i + 1})`)
-                  .classList.remove('active');
-            }
-         });
-         let matches = towns.filter(x => x.includes(inputValue));
-         resultDiv.textContent = `${matches.length} matches found`;
-      }
-   }
-
-   function update() {
-      render(ulTemplate(), townsDiv);
-   }
+function update(match = '') {
+   render(articleTemplate(towns, match), body);
 }
-search();
+update();
+
+function onSearch(e) {
+   e.preventDefault();
+   let match = document.querySelector('#searchText').value;
+   update(match);
+   document.querySelector('#result').textContent = `${countMatches(match)} matches found`;
+}
+
+function countMatches(match) {
+   if (match == '') return 0;
+   return towns.filter(x => x.includes(match)).length;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { html, render } from '../node_modules/lit-html/lit-html.js';
+// import { styleMap } from '../node_modules/lit-html/directives/style-map.js';
+// import { towns } from './towns.js';
+
+// function search() {
+//    const input = document.querySelector('#searchText');
+//    const resultDiv = document.querySelector('#result');
+//    const searchBtn = document.querySelector('button');
+//    const townsDiv = document.querySelector('#towns');
+//    const ulTemplate = () => html`
+//       <ul>
+//          ${towns.map(x => html`<li>${x}</li>`)}
+//       </ul>
+//    `;
+//    update();
+
+//    searchBtn.addEventListener('click', onSearch);
+
+//    function onSearch(e) {
+//       e.preventDefault();
+//       let inputValue = input.value.trim();
+//       if (inputValue != '') {
+//          towns.forEach((x, i) => {
+//             if (x.includes(inputValue)) {
+//                document.querySelector(`li:nth-of-type(${i + 1})`)
+//                   .classList.add('active');
+//             } else {
+//                document.querySelector(`li:nth-of-type(${i + 1})`)
+//                   .classList.remove('active');
+//             }
+//          });
+//          let matches = towns.filter(x => x.includes(inputValue));
+//          resultDiv.textContent = `${matches.length} matches found`;
+//       }
+//    }
+
+//    function update() {
+//       render(ulTemplate(), townsDiv);
+//    }
+// }
+// search();
