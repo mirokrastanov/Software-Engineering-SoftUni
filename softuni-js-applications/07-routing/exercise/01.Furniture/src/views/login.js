289071086979faf32126f54569c79dc4aeeb3e60
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-03-24 14:07:05 +0200

    login implemented

--- softuni-js-applications/07-routing/exercise/01.Furniture/src/views/login.js
+++ softuni-js-applications/07-routing/exercise/01.Furniture/src/views/login.js
@@ -1,7 +1,40 @@
 import { html, render } from '../../node_modules/lit-html/lit-html.js';
 import * as api from '../api/api.js';
+import { login } from '../api/auth.js';
 
 
-export async function loginPage() {
-    console.log('login');
+export async function loginPage(ctx) {
+    ctx.render(loginTemplate(onSubmit));
+}
+
+function onSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const { email, password } = Object.fromEntries(formData);
+    login(email, password);
+}
+
+function loginTemplate(handler) { // handler ===> onSubmit()
+    return html`
+    <div class="row space-top">
+        <div class="col-md-12">
+            <h1>Login User</h1>
+            <p>Please fill all fields.</p>
+        </div>
+    </div>
+    <form @submit=${handler}>
+        <div class="row space-top">
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label class="form-control-label" for="email">Email</label>
+                    <input class="form-control" id="email" type="text" name="email">
+                </div>
+                <div class="form-group">
+                    <label class="form-control-label" for="password">Password</label>
+                    <input class="form-control" id="password" type="password" name="password">
+                </div>
+                <input type="submit" class="btn btn-primary" value="Login" />
+            </div>
+        </div>
+    </form>`;
 }
