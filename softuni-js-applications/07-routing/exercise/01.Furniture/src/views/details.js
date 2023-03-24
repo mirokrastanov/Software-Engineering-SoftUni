55599bde50f3e7fda43ce17ac8a0ae5940fb46c7
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-03-24 15:29:34 +0200

    details rendering implemented

--- softuni-js-applications/07-routing/exercise/01.Furniture/src/views/details.js
+++ softuni-js-applications/07-routing/exercise/01.Furniture/src/views/details.js
@@ -1,6 +1,45 @@
 import { html, render } from '../../node_modules/lit-html/lit-html.js';
 import * as api from '../api/api.js';
+import { getItemById } from '../api/auth.js';
 
-export async function detailsPage() {
-    console.log('details');
+let context = null;
+export async function detailsPage(ctx) {
+    const id = ctx.params.id;
+    const item = await getItemById(id);
+    console.log(item);
+    const userData = JSON.parse(localStorage.getItem('userData'));
+
+    ctx.render(detailsTemplate(item, userData._id == item._ownerId));
+
+}
+
+function detailsTemplate(item, isOwner) {
+    return html`
+    <div class="row space-top">
+        <div class="col-md-12">
+            <h1>Furniture Details</h1>
+        </div>
+    </div>
+    <div class="row space-top">
+        <div class="col-md-4">
+            <div class="card text-white bg-primary">
+                <div class="card-body">
+                    <img src="/${item.img}" />
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <p>Make: <span>${item.make}</span></p>
+            <p>Model: <span>${item.model}</span></p>
+            <p>Year: <span>${item.year}</span></p>
+            <p>Description: <span>${item.description}</span></p>
+            <p>Price: <span>${item.price}</span></p>
+            <p>Material: <span>${item.material}</span></p>
+            ${ isOwner ? html`
+            <div>
+                <a href=”#” class="btn btn-info">Edit</a>
+                <a href=”#” class="btn btn-red">Delete</a>
+            </div>` : null }
+        </div>
+    </div>`;
 }
