1ba39215037eccf5f4d9dc7b39b14ee924116959
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-03-24 12:42:38 +0200

    routing structure

---
+++ softuni-js-applications/07-routing/exercise/01.Furniture/src/views/login.js
@@ -0,0 +1,7 @@
+import { page } from './node_modules/page/page.mjs';
+import { html, render } from './node_modules/lit-html/lit-html.js';
+import * as api from './src/api/api.js';
+
+export async function loginPage() {
+    console.log('login');
+}
