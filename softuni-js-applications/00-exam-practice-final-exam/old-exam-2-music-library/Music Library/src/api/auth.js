a2710b06065c13e6ee5253eee479b4459218103d
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-03-30 15:15:22 +0300

    Auth module completed

--- softuni-js-applications/00-exam-practice-final-exam/old-exam-2-music-library/Music Library/src/api/auth.js
+++ softuni-js-applications/00-exam-practice-final-exam/old-exam-2-music-library/Music Library/src/api/auth.js
@@ -4,7 +4,13 @@ const endpoints = {
     'login': 'users/login',
     'register': 'users/register',
     'logout': 'users/logout',
-    'allAlbums': 'data/albums?sortBy=_createdOn%20desc'
+    'allAlbums': 'data/albums?sortBy=_createdOn%20desc',
+    'addAlbum': 'data/albums',
+    'details': 'data/albums/', // :id
+    'likeAlbum': 'data/likes',
+    'getTotalLikes': 'data/likes?where=albumId%3D%22', // + id + '%22&distinct=_ownerId&count'
+    'getMyLikes': 'data/likes?where=albumId%3D%22', // + id + '%22%20and%20_ownerId%3D%22' + userId + '%22&count'
+
 };
 
 export async function login(email, password) {
@@ -30,30 +36,41 @@ export async function getAllAlbums() {
     return res;
 }
 
-// export async function getMemeDetailsById(id) {
-//     const res = await api.get(endpoints.memeDetails + id);
-//     return res;
-// }
+export async function addNewAlbum(data) {
+    const res = await api.post(endpoints.addAlbum, data);
+    return res;
+}
 
-// export async function updateMemeById(id, data) {
-//     const res = await api.put(endpoints.memeDetails + id, data);
-//     return res;
-// }
+export async function getAlbumDetailsById(id) {
+    const res = await api.get(endpoints.details + id);
+    return res;
+}
 
-// export async function createMeme(data) {
-//     const res = await api.post(endpoints.createMeme, data);
-//     return res;
-// }
+export async function likeAlbum(id) {
+    const res = await api.post(endpoints.likeAlbum, { albumId: id });
+    return res;
+}
 
-// export async function deleteMemeById(id) {
-//     const res = await api.del(endpoints.memeDetails + id);
-//     return res;
-// }
+export async function getTotalLikes(id) {
+    const res = await api.get(endpoints.getTotalLikes + id + '%22&distinct=_ownerId&count');
+    return res;
+}
 
-// export async function getMyMemes() {
-//     const userData = JSON.parse(localStorage.getItem('userData'));
-//     const userId = userData._id;
-//     const url = endpoints.myMemes + userId + '%22&sortBy=_createdOn%20desc';
-//     const res = await api.get(url);
-//     return res;
-// }
+export async function getMyLikes(id) {
+    const userData = JSON.parse(localStorage.getItem('userData'));
+    const userId = userData._id;
+    const add1 = '%22%20and%20_ownerId%3D%22';
+    const add2 = '%22&count';
+    const res = await api.get(endpoints.getMyLikes + id + add1 + userId + add2);
+    return res;
+}
+
+export async function editAlbum(id, data) {
+    const res = await api.put(endpoints.details + id, data);
+    return res;
+}
+
+export async function deleteAlbum(id) {
+    const res = await api.del(endpoints.details + id);
+    return res;
+}
