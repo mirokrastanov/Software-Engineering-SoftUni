5cd0e09b68c68ac3b47115a772660bda0e093b37
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-05-15 17:08:10 +0300

    Cats now render dynamically

--- softuni-js-back-end/01-node.js-intro/exercise/cat-shelter-project/index.js
+++ softuni-js-back-end/01-node.js-intro/exercise/cat-shelter-project/index.js
@@ -1,17 +1,21 @@
 const http = require('http');
-const { indexTemplate, citeCss, } = require('./views/templates.js');
+const { db } = require('./database.js');
+const { indexTemplate, citeCss, indexCatTemplate, } = require('./views/templates.js');
 
 
 const server = http.createServer(async (req, res) => {
     console.log('Server is called at: ' + req.url);
 
     if (req.url == '/' || req.url == '/index.html' || req.url == '/index') {
+        const catsHtml = db.cats.map(cat => indexCatTemplate.replace('{{catName}}', cat.name)).join('');
+        const indexHtml = indexTemplate.replace('{{cats}}', catsHtml);
+
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(indexTemplate);
+        res.write(indexHtml);
     } else if (req.url == '/cats/add-breed') {
 
     } else if (req.url == '/cats/add-cat') {
-
+        
     } else if (req.url == '/cat-shelter') {
 
     } else if (req.url == '/cats/edit-cat') {
