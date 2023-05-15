1d3e370f2b8aa42fb8f54435a5f2a52140e97f7a
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-05-15 17:56:12 +0300

    DB updated with breeds

--- softuni-js-back-end/01-node.js-intro/exercise/cat-shelter-project/database.js
+++ softuni-js-back-end/01-node.js-intro/exercise/cat-shelter-project/database.js
@@ -5,24 +5,60 @@ exports.db = {
             name: 'Spike',
             breed: 'Persian',
             description: 'The Persian cat, also known as the Persian longhair, is a long-haired breed of cat characterized by a round face and short muzzle.',
+            img: 'https://www.thesprucepets.com/thmb/1Q3ZQVxADUQ2Aam4mhSS8FzqKP4=/2164x0/filters:no_upscale():strip_icc()/GettyImages-904624038-8a3d9763601742258dd48df05e1ce69f.jpg',
         },
         {
             id: 1,
             name: 'Arny',
             breed: 'Angora',
             description: 'The Turkish Angora is a breed of domestic cat. Turkish Angoras are one of the ancient, natural breeds of cat, having originated in central Anatolia.',
+            img: 'https://cats.com/wp-content/uploads/2020/10/Turkish-Angora-Cat-compressed-768x384.jpg',
         },
         {
             id: 2,
             name: 'Bennu',
             breed: 'Abyssinian',
             description: 'The Abyssinian is a lithe cat with relatively slender legs and a long, tapering tail.',
+            img: 'https://www.thesprucepets.com/thmb/MigiLYeVSJcj0zkNVkJ--rALMZI=/2121x0/filters:no_upscale():strip_icc()/GettyImages-165827729-efc11c02690f457a81ef6ccbfa8eb34d.jpg',
         },
         {
             id: 3,
             name: 'Cuddly',
             breed: 'Siamese',
             description: 'The long-haired Siamese is recognized internationally as a Balinese cat. Siamese cats are one of the more common breeds to have non-white irises.',
+            img: 'https://catastic.b-cdn.net/wp-content/uploads/2023/03/siamese-cat-price.jpg',
+        },
+    ],
+    breeds: [
+        {
+            name: 'Persian',
+        },
+        {
+            name: 'Angora',
+        },
+        {
+            name: 'Abyssianian',
+        },
+        {
+            name: 'Siamese',
+        },
+        {
+            name: 'British Shorthair',
+        },
+        {
+            name: 'Maine Coon',
+        },
+        {
+            name: 'Ragdoll',
+        },
+        {
+            name: 'Sphynx',
+        },
+        {
+            name: 'Scottish Fold',
+        },
+        {
+            name: 'Birman',
         },
     ],
 };
