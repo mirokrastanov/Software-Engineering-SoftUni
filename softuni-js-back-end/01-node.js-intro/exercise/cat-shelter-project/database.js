e33ecee3308f016862b1cb3d7bbd0e9279ca5946
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-05-15 17:07:17 +0300

    Added local database

---
+++ softuni-js-back-end/01-node.js-intro/exercise/cat-shelter-project/database.js
@@ -0,0 +1,29 @@
+exports.db = {
+    cats: [
+        {
+            id: 0,
+            name: 'Spike',
+            breed: 'Persian',
+            description: 'The Persian cat, also known as the Persian longhair, is a long-haired breed of cat characterized by a round face and short muzzle.',
+        },
+        {
+            id: 1,
+            name: 'Arny',
+            breed: 'Angora',
+            description: 'The Turkish Angora is a breed of domestic cat. Turkish Angoras are one of the ancient, natural breeds of cat, having originated in central Anatolia.',
+        },
+        {
+            id: 2,
+            name: 'Bennu',
+            breed: 'Abyssinian',
+            description: 'The Abyssinian is a lithe cat with relatively slender legs and a long, tapering tail.',
+        },
+        {
+            id: 3,
+            name: 'Cuddly',
+            breed: 'Siamese',
+            description: 'The long-haired Siamese is recognized internationally as a Balinese cat. Siamese cats are one of the more common breeds to have non-white irises.',
+        },
+    ],
+};
+
