fcf3570ffe3b28358ea39ea370783732a146b9d2
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-05-15 14:42:00 +0300

    change dir

--- softuni-js-back-end/01-node.js-intro/demo/module.js
+++
@@ -1,4 +0,0 @@
-console.log(this === global);
-console.log(this);
-// console.log(global);
-// console.log(globalThis === global)
--- softuni-js-back-end/01-node.js-intro/demo/script.js
+++
@@ -1,6 +0,0 @@
-let name = 'Pesho';
-let age = 20;
-
-let info = `I am ${name} and I am ${age} years old`;
-
-console.log(info);
