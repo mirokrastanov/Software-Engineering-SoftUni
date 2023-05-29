858c5d8c5ed73d7e3ecc518327d8f0eb3fe08390
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-05-29 18:07:11 +0300

    express server expansion

--- softuni-js-back-end/03-express.js-and-templating/lab/practice-demo/demo-1.js
+++ softuni-js-back-end/03-express.js-and-templating/lab/practice-demo/demo-1.js
@@ -1,18 +1,56 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
+// Add third party middleware
+const bodyParser = express.urlencoded({ extended: false });
+app.use(bodyParser);
+
+
+
+// Add middlewares
+// Global middlewares
+app.use((req, res, next) => {
+    console.log('Middleware 1 - User connecting to server...');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log(`HTTP Request ${req.method}: ${req.path}`);
+    next();
+});
+
+// Partial route middlewares
+app.use('/cats', (req, res, next) => {
+    console.log('Cats middleware');
+    next();
+});
+
+// Route specific middleware
+const specificMiddleware = (req, res, next) => {
+    console.log('Specific middleware');
+    next();
+};
+app.get('/specific', specificMiddleware, (req, res) => {
+    res.send('Some specific route with middleware');
+});
+
+
+
+
+
+// Express Router / Actions
 app.get('/', (req, res) => {
     res.send('Req body here...');
 });
 
-app.post('/', (req, res) => {
-    res.status(201).send('Cat has been created!');
-});
-
 app.get('/cats', (req, res) => {
     res.send('Displays cats here.');
+});
+
+app.post('/cats', (req, res) => {
+    console.log(req.body);
+    res.status(201).send('Cat has been created!');
 });
 
 app.get('/cats/:catId', (req, res) => {
@@ -42,5 +80,7 @@ app.get('/old-route', (req, res) => {
 app.get('*', (req, res) => {
     res.status(404).send('Page not found');
 });
+// End of Express Router
+
 
 app.listen(5000, () => console.log('Server is listening on port 5000...'));
