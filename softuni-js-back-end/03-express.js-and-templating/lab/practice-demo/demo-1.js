d39eda74fee81480fc42a849b472c62ea274e936
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-05-29 14:22:48 +0300

    server handlers

--- softuni-js-back-end/03-express.js-and-templating/lab/practice-demo/demo-1.js
+++ softuni-js-back-end/03-express.js-and-templating/lab/practice-demo/demo-1.js
@@ -1,9 +1,46 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Req body here...');
+});
+
+app.post('/', (req, res) => {
+    res.status(201).send('Cat has been created!');
+});
+
+app.get('/cats', (req, res) => {
+    res.send('Displays cats here.');
+});
+
+app.get('/cats/:catId', (req, res) => {
+    const catId = Number(req.params.catId);
+    if (!catId) return res.status(404).send('Cannot find cat.');
+
+    console.log(req.params);
+    res.send(`Request with parameter - ${req.params.catId}.`);
+});
+
+app.get('/download', (req, res) => {
+    res.download('./files/sample.pdf');
+
+    // res.attachment('./files/sample.pdf');
+    // res.end();
+
+    // res.sendFile(path.resolve(__dirname, 'files/sample.pdf'));
+});
+
+app.get('/old-route', (req, res) => {
+    res.redirect('/cats');
+});
+
+
+
+
+app.get('*', (req, res) => {
+    res.status(404).send('Page not found');
 });
 
 app.listen(5000, () => console.log('Server is listening on port 5000...'));
