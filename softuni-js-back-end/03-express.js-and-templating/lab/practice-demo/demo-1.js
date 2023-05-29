faf573f312fd7b12882635e95f4c1f2077161759
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-05-29 13:10:31 +0300

    server created + start script

--- softuni-js-back-end/03-express.js-and-templating/lab/practice-demo/demo-1.js
+++ softuni-js-back-end/03-express.js-and-templating/lab/practice-demo/demo-1.js
@@ -1,1 +1,9 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Req body here...');
+});
+
+app.listen(5000, () => console.log('Server is listening on port 5000...'));
