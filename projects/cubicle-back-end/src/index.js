fedc97821c9d4d593bbc7afa5f312fa534667d81
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-05-31 13:19:39 +0300

    Add express to project

--- projects/cubicle-back-end/src/index.js
+++ projects/cubicle-back-end/src/index.js
@@ -1,2 +1,13 @@
-console.log('Hello from Express!');
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello from Express!');
+});
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
+
+
+
 
