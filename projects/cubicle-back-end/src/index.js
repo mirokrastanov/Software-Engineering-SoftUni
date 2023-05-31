6ca9eba0c3caf2e7e539e103b0ccd6f7fb0430b2
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-05-31 14:26:21 +0300

    Add cube controller

--- projects/cubicle-back-end/src/index.js
+++ projects/cubicle-back-end/src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const expressConfigurator = require('./config/expressConfigurator');
 const handlebarsConfigurator = require('./config/handlebarsConfigurator');
 const homeController = require('./controllers/homeController');
+const cubeController = require('./controllers/cubeController');
 
 const app = express();
 const PORT = 5000;
@@ -11,6 +12,7 @@ expressConfigurator(app);
 handlebarsConfigurator(app);
 
 app.use(homeController);
+app.use('/cubes', cubeController);
 
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
