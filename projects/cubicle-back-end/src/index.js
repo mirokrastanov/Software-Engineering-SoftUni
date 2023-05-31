4f465250473327b46421e57d307155c8d8b1bc8b
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-05-31 18:18:53 +0300

    Extract routes

--- projects/cubicle-back-end/src/index.js
+++ projects/cubicle-back-end/src/index.js
@@ -2,20 +2,14 @@ const express = require('express');
 
 const expressConfigurator = require('./config/expressConfigurator');
 const handlebarsConfigurator = require('./config/handlebarsConfigurator');
-const homeController = require('./controllers/homeController');
-const cubeController = require('./controllers/cubeController');
+const routes = require('./routes');
 
 const app = express();
 const PORT = 5000;
 
 expressConfigurator(app);
 handlebarsConfigurator(app);
-
-app.use(homeController);
-app.use('/cubes', cubeController);
-app.get('*', (req, res) => {
-    res.redirect('/404');
-});
+app.use(routes);
 
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
