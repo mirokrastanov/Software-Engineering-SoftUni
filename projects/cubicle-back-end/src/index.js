df5352cf20a4997e35e726fc625cec04917c4962
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-05-31 14:06:34 +0300

    Rendered About page

--- projects/cubicle-back-end/src/index.js
+++ projects/cubicle-back-end/src/index.js
@@ -7,18 +7,10 @@ const homeController = require('./controllers/homeController');
 const app = express();
 const PORT = 5000;
 
-//Express config
 expressConfigurator(app);
-
-// Handlebars config
 handlebarsConfigurator(app);
 
-// Routers
 app.use(homeController);
 
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
-
-
-
-
