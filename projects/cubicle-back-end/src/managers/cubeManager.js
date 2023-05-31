e0d8ce6cea5ba116804766bca81a8f9d180b31b5
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-05-31 17:56:26 +0300

    Cube details update

--- projects/cubicle-back-end/src/managers/cubeManager.js
+++ projects/cubicle-back-end/src/managers/cubeManager.js
@@ -1,5 +1,21 @@
 const uniqid = require('uniqid');
-const cubes = [];
+const cubes = [
+    // 2 mock items 
+    {
+        id: '32l4hjikgb435kbhgo3ik',
+        name: 'Rubic Classic',
+        description: 'Evergreen',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0482/9177/4628/products/o3rmamdfu2kartbxpul9.jpg?v=1681761411',
+        difficultyLevel: 3,
+    },
+    {
+        id: 'p1mdsl4hj5m6p56pmpgok',
+        name: 'Yarie',
+        description: 'Hexagonal Rubic\'s Cube',
+        imageUrl: 'https://m.media-amazon.com/images/I/61r6XfeM5HL.jpg',
+        difficultyLevel: 6,
+    },
+];
 
 exports.getAll = () => cubes.slice();
 
