eb6423eb26591587cce099cfafaa76f7f947c0f7
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-06-02 19:59:18 +0300

    Show remaining accessories only

--- projects/cubicle-back-end/src/managers/accessoryManager.js
+++ projects/cubicle-back-end/src/managers/accessoryManager.js
@@ -3,3 +3,5 @@ const Accessory = require('../models/Accessory');
 exports.getAll = () => Accessory.find();
 
 exports.create = (accessoryData) => Accessory.create(accessoryData);
+
+exports.getNotAttached = (accessoryIds) => Accessory.find({ _id: { $nin: accessoryIds } }); // find all, that are not already inside the accessoryIds array
