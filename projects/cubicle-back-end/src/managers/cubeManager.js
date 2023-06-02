66db69a78ca9f22caca7aa4dd2ad0064312714f2
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-06-02 15:10:55 +0300

    Get cubes from DB

--- projects/cubicle-back-end/src/managers/cubeManager.js
+++ projects/cubicle-back-end/src/managers/cubeManager.js
@@ -1,26 +1,9 @@
 const Cube = require('../models/Cube');
 
-const uniqid = require('uniqid');
-const cubes = [
-    // 2 mock items 
-    {
-        id: '32l4hjikgb435kbhgo3ik',
-        name: 'Rubic Classic',
-        description: 'Evergreen',
-        imageUrl: 'https://cdn.shopify.com/s/files/1/0482/9177/4628/products/o3rmamdfu2kartbxpul9.jpg?v=1681761411',
-        difficultyLevel: 3,
-    },
-    {
-        id: 'p1mdsl4hj5m6p56pmpgok',
-        name: 'Yarie',
-        description: 'Hexagonal Rubic\'s Cube',
-        imageUrl: 'https://m.media-amazon.com/images/I/61r6XfeM5HL.jpg',
-        difficultyLevel: 6,
-    },
-];
+exports.getAll = async (search, from, to) => {
+    let result = await Cube.find().lean();
 
-exports.getAll = (search, from, to) => {
-    let result = cubes.slice();
+    // TODO: Use mongoose to filter in the db
     if (search) {
         result = result.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
     }
@@ -33,7 +16,7 @@ exports.getAll = (search, from, to) => {
     return result;
 };
 
-exports.getOne = (cubeId) => cubes.find(cube => cube.id == cubeId);
+exports.getOne = (cubeId) => Cube.findById(cubeId);
 
 exports.create = async (cubeData) => {
     // const newCube = {
