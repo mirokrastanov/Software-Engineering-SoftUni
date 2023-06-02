30c4d3eeef60627a552b59b737e3eb94c4607af1
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-06-02 18:56:06 +0300

    Show cube and accessories on attach page

--- projects/cubicle-back-end/src/controllers/cubeController.js
+++ projects/cubicle-back-end/src/controllers/cubeController.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const cubeManager = require('../managers/cubeManager');
+const accessoryManager = require('../managers/accessoryManager');
 
 router.get('/create', (req, res) => {
     console.log(cubeManager.getAll());
@@ -24,14 +25,17 @@ router.post('/create', async (req, res) => {
 
 router.get('/:cubeId/details', async (req, res) => {
     const cube = await cubeManager.getOne(req.params.cubeId).lean();
-    
+
     if (!cube) return res.redirect('/404');
 
     res.render('details', { cube });
 });
 
-router.get('/:cubeId/attach-accessory', (req, res) => {
-    res.render('accessory/attach');
+router.get('/:cubeId/attach-accessory', async (req, res) => {
+    const cube = await cubeManager.getOne(req.params.cubeId).lean();
+    const accessories = await accessoryManager.getAll().lean();
+
+    res.render('accessory/attach', { cube, accessories });
 });
 
 
