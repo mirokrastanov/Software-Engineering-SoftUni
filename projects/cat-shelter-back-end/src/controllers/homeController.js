80d6e5fb457c58dac72c63e158bd81b0f90a93a7
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-06-05 14:01:41 +0300

    Added dynamic search partial layout condition

--- projects/cat-shelter-back-end/src/controllers/homeController.js
+++ projects/cat-shelter-back-end/src/controllers/homeController.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 
 router.get('/', async (req, res) => {
-    res.render('index');  // loads index.hbs and loads it with the "items" array
+    res.render('index', { search: true });  // loads index.hbs and loads it with the "items" array
 });
 
 router.get('/404', (req, res) => {
