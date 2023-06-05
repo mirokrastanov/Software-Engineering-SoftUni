789e65c3c88d3e86fec514c0fbcf14ac7875634d
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-06-05 18:19:17 +0300

    Connect to database via mongoose

--- projects/cat-shelter-back-end/src/index.js
+++ projects/cat-shelter-back-end/src/index.js
@@ -2,10 +2,8 @@ const express = require('express');
 
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
-
+const dbConnect = require('./config/dbConfig');
 const routes = require('./routes');
-
-
 
 const app = express();
 const PORT = 5000;
@@ -13,7 +11,9 @@ const PORT = 5000;
 expressConfig(app);
 handlebarsConfig(app);
 
-
+dbConnect()
+    .then(() => console.log('DB Connected successfully!'))
+    .catch(err => console.log('DB Error', err))
 
 app.use(routes);
 
