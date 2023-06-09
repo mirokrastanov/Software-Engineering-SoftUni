b27a652c6c03d95d354d0b418afc4362ea19ba13
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-06-09 15:11:21 +0300

    User authentication middleware

--- projects/cubicle-back-end/src/middlewares/authMiddleware.js
+++ projects/cubicle-back-end/src/middlewares/authMiddleware.js
@@ -10,6 +10,8 @@ exports.auth = async (req, res, next) => {
             const decodedToken = await jwt.verify(token, SECRET);
 
             req.user = decodedToken; // save info about the user inside the request object as it is transfered futher to the next function as well
+            res.locals.user = decodedToken;
+            res.locals.isAuthenticated = true; 
 
             next();
         } catch (error) {
