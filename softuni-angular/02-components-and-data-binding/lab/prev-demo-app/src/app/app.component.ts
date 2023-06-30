f4cde79c6228f3ad8308589593e3635c87ac261a
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-06-30 15:02:16 +0300

    Add reference & binding to the app component

--- softuni-angular/02-components-and-data-binding/lab/prev-demo-app/src/app/app.component.ts
+++ softuni-angular/02-components-and-data-binding/lab/prev-demo-app/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo2';
+  title = 'proj';
+
+  changeTitleHandler(newTitle: string) {
+    this.title = newTitle;
+  }
 }
