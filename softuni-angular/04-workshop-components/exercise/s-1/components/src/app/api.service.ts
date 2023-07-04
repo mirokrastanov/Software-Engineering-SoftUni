81c577d4cbc08d9bbe3225b6a530458174fef2dc
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-04 14:17:24 +0300

    Fetch data from mongo db

---
+++ softuni-angular/04-workshop-components/exercise/s-1/components/src/app/api.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+  getThemes() {
+    const { appUrl } = environment;
+    return this.http.get(`${appUrl}/themes`);
+  }
+  getPosts(limit?: number) {
+    const { appUrl } = environment;
+    return this.http.get(`${appUrl}/posts${limit ? `?limit=${limit}` : ''}`);
+  }
+}
