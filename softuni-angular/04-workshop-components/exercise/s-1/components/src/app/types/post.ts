e63193b1891c6ef6987bcfdbd4eb2c471917c646
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-04 14:58:47 +0300

    Add static site content for all views

--- softuni-angular/04-workshop-components/exercise/s-1/components/src/app/types/post.ts
+++ softuni-angular/04-workshop-components/exercise/s-1/components/src/app/types/post.ts
@@ -1,22 +1,12 @@
-import { UserId } from "./theme";
-
-export interface ThemeId {
-    subscribers: string[];
-    posts: string[];
-    _id: string;
-    themeName: string;
-    userId: string;
-    created_at: string;
-    updatedAt: string;
-    __v: number;
-}
+import { Theme } from "./theme";
+import { UserId } from "./user-id";
 
 export interface Post {
     likes: string[];
     _id: string;
     text: string;
     userId: UserId;
-    themeId: ThemeId;
+    themeId: Theme;
     created_at: string;
     updatedAt: string;
     __v: number;
