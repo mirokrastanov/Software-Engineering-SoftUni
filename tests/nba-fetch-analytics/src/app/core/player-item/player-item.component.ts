3f5147a1926144b1adc66a0bca3989b9dad9ed59
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-13 18:13:01 +0300

    Add more dynamic info

--- tests/nba-fetch-analytics/src/app/core/player-item/player-item.component.ts
+++ tests/nba-fetch-analytics/src/app/core/player-item/player-item.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Player } from 'src/app/util/types';
 
 @Component({
   selector: 'app-player-item',
   templateUrl: './player-item.component.html',
   styleUrls: ['./player-item.component.css']
 })
-export class PlayerItemComponent {
+export class PlayerItemComponent implements OnInit {
+  @Input() player!: Player; 
+
+  ngOnInit(): void {
+    console.log(this.player);
+    
+  }
 
 }
--- tests/nba-fetch-analytics/src/app/core/players-table/players-table.component.ts
+++ tests/nba-fetch-analytics/src/app/core/players-table/players-table.component.ts
@@ -6,20 +6,23 @@ import { ApiService } from 'src/app/services/api.service';
   templateUrl: './players-table.component.html',
   styleUrls: ['./players-table.component.css']
 })
-export class PlayersTableComponent implements OnInit{
+export class PlayersTableComponent implements OnInit {
   constructor(private apiService: ApiService) { }
-  dataArray: any;
+  playersArray: any;
   metaObject: any;
 
   ngOnInit(): void {
     this.apiService.fetchData('players').subscribe({
       next: (data) => {
-        this.dataArray = data.data;
+        this.playersArray = data.data;
         this.metaObject = data.meta;
         console.log(data);
         console.log(data.data[0]);
         let test: object = {};
         console.log(test.hasOwnProperty('players'));
+
+       
+        
 
       }
     });
