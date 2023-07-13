c28d735ca36fcd020898ab1c719afa7800eba50d
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-13 19:40:53 +0300

    Routing fully working & styled

--- tests/nba-fetch-analytics/src/app/app-routing.module.ts
+++ tests/nba-fetch-analytics/src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { PlayersTableComponent } from './core/players-table/players-table.component';
 import { TeamsComponent } from './core/teams/teams.component';
 import { StatsComponent } from './core/stats/stats.component';
+import { HomeComponent } from './core/home/home.component';
+import { ErrorComponent } from './core/error/error.component';
 
-const routes: Routes = [
+const routes: Routes = [ // UPPER/LOWER CASE MATTERS !!!!!!! routerLink in the html should MATCH IT!!!
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: PlayersTableComponent },
+  { path: 'home', component: HomeComponent}, 
   { path: 'players', component: PlayersTableComponent },
   { path: 'teams', component: TeamsComponent },
   { path: 'stats', component: StatsComponent },
+  { path: '**', component: ErrorComponent }, // 404
 ];
 
 @NgModule({
