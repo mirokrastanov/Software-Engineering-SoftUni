915a6c0e7278aad5f355a12806a12f4f30627cfd
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-13 16:53:08 +0300

    Add component

---
+++ tests/nba-fetch-analytics/src/app/core/players-table/players-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-players-table',
+  templateUrl: './players-table.component.html',
+  styleUrls: ['./players-table.component.css']
+})
+export class PlayersTableComponent {
+
+}
