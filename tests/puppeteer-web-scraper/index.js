964541e408591662ad42a3f815440712bf63121e
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-19 21:52:57 +0300

    Reconstruct standings data into a JS table

--- tests/puppeteer-web-scraper/index.js
+++ tests/puppeteer-web-scraper/index.js
@@ -17,14 +17,21 @@ async function scrapeProduct(url) {
 
     const bodyEl1 = await page.waitForSelector('.Crom_body__UYOcU');
     const bodyTdArray1 = await bodyEl1.$$('td');
-    const bodyArray1 = [];
+    const bodyArray1 = { temp: {}, result: [] };
     for (let i = 0; i < bodyTdArray1.length; i++) {
         const element = bodyTdArray1[i];
         const thText = await page.evaluate(element => element.textContent, element);
-        bodyArray1.push(thText);
-    }
-    console.log(bodyArray1);
+        bodyArray1.temp[headersArray1[i % 12]] = thText;
 
+        if (Object.keys(bodyArray1.temp).length == 12) {
+            let copy = JSON.parse(JSON.stringify(bodyArray1.temp));
+            bodyArray1.result.push(copy);
+            bodyArray1.temp = {};
+        }       
+    }
+    console.table(new Array(2).fill({ x: 1, y: 2, z: 3 }));
+    console.table(bodyArray1.result, headersArray1);
+    console.log(bodyArray1.result);
 
 
 
