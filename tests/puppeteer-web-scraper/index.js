78059d9b4a76672c084593af4eb25ecb5d6dd38e
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-07-19 20:53:20 +0300

    Scraped the table body of the standings

--- tests/puppeteer-web-scraper/index.js
+++ tests/puppeteer-web-scraper/index.js
@@ -5,16 +5,25 @@ async function scrapeProduct(url) {
     const page = await browser.newPage();
     await page.goto(url);
 
-    const headerRow1 = await page.waitForSelector('.Crom_headers__mzI_m')
-    const thArray1 = await headerRow1.$$('th');
+    const headersRowEl1 = await page.waitForSelector('.Crom_headers__mzI_m');
+    const headersThArray1 = await headersRowEl1.$$('th');
     const headersArray1 = [];
-    for (let i = 0; i < thArray1.length; i++) {
-        const element = thArray1[i];
+    for (let i = 0; i < headersThArray1.length; i++) {
+        const element = headersThArray1[i];
         const thText = await page.evaluate(element => element.textContent, element);
         headersArray1.push(thText);
     }
     console.log(headersArray1);
 
+    const bodyEl1 = await page.waitForSelector('.Crom_body__UYOcU');
+    const bodyTdArray1 = await bodyEl1.$$('td');
+    const bodyArray1 = [];
+    for (let i = 0; i < bodyTdArray1.length; i++) {
+        const element = bodyTdArray1[i];
+        const thText = await page.evaluate(element => element.textContent, element);
+        bodyArray1.push(thText);
+    }
+    console.log(bodyArray1);
 
 
 
