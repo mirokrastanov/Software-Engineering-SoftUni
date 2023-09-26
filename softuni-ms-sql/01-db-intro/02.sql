8b52281e0735c27923655b7f42dff5b0e4a3ab4d
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-09-26 18:46:05 +0300

    sql exercise

---
+++ softuni-ms-sql/01-db-intro/02.sql
@@ -0,0 +1,14 @@
+-- Problem 02
+USE [Minions]
+
+CREATE TABLE [Minions] (
+	[Id] INT PRIMARY KEY,
+	[Name] NVARCHAR(50) NOT NULL,
+	[Age] INT NOT NULL
+)
+
+CREATE TABLE [Towns] (
+	[Id] INT PRIMARY KEY,
+	[Name] NVARCHAR(70) NOT NULL,
+)
+
