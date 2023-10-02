94271c5cad18de8af291fd80e278b16c5912c211
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-10-02 16:34:33 +0300

    SQL Exercise

---
+++ softuni-ms-sql/03-table-relations/09.sql
@@ -0,0 +1,10 @@
+USE [Geography]
+
+SELECT
+	[MountainRange],
+	[PeakName],
+	[Elevation]
+FROM [Mountains] AS m
+JOIN [Peaks] AS p ON m.[Id] = p.[MountainId]
+WHERE [MountainRange] = 'Rila'
+ORDER BY [Elevation] DESC
