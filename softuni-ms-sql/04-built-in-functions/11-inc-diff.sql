0a147085bf13848a46fbec1f7ebee394c9c8d6b3
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-10-03 19:31:37 +0300

    Alt solution

--- softuni-ms-sql/04-built-in-functions/11-inc-diff.sql
+++ softuni-ms-sql/04-built-in-functions/11-inc-diff.sql
@@ -11,3 +11,12 @@ SELECT * FROM
 	) AS [Employees] -- The subquery needs to have an alias to work!
 WHERE [Salary] BETWEEN 10000 AND 50000 AND [Rank] = 2
 ORDER BY [Salary] DESC;
+
+-- Alt solution
+SELECT * FROM 
+	(SELECT [EmployeeID], [FirstName], [LastName], [Salary],
+	DENSE_RANK() OVER (PARTITION BY [Salary] ORDER BY [EmployeeID]) AS [Rank]
+	FROM [Employees]
+	WHERE [Salary] BETWEEN 10000 AND 50000) AS [Subquery]
+WHERE [Subquery].[Rank] = 2
+ORDER BY [Salary] DESC;
