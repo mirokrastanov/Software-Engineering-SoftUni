5c2bc6b8a5b0d5dc758e036e8d123260e3fb62dc
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-10-04 19:33:57 +0300

    SQL Exercise

---
+++ softuni-ms-sql/05-subqueries-and-joins/08.sql
@@ -0,0 +1,10 @@
+SELECT
+	e.[EmployeeID],
+	e.[FirstName],
+	CASE
+		WHEN DATEPART(YEAR, p.[StartDate]) >= 2005 THEN NULL
+		ELSE p.[Name]
+	END AS [ProjectName]
+FROM [Employees] AS e
+JOIN [EmployeesProjects] AS ep ON e.[EmployeeID] = ep.[EmployeeID] AND e.[EmployeeID] = 24
+JOIN [Projects] AS p ON ep.[ProjectID] = p.[ProjectID];
