0e120b3b06452c8aedafa4f5f223b80b2b835490
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-10-14 12:11:11 +0300

    SQL Exam prep

---
+++ softuni-ms-sql/00-exam-prep/05-10-dec-2021/02.sql
@@ -0,0 +1,6 @@
+INSERT INTO Passengers(FullName, Email)
+	SELECT
+		CONCAT_WS(' ', FirstName, LastName) AS FullName,
+		CONCAT_WS('', FirstName, LastName, '@gmail.com') AS Email
+	FROM Pilots
+	WHERE Id BETWEEN 5 AND 15;
