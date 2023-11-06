e61743d7ba0d3e96a23ae4d0d9cc7fc4d637d81c
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-11-06 19:59:10 +0200

    Implement a Controlled form

--- softuni-react-js/05-forms/demo1/src/App.jsx
+++ softuni-react-js/05-forms/demo1/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import UncontrolledForm from './components/UncontrolledForm'
+import ControlledForm from './components/ControlledForm'
 import './App.css'
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
 
   return (
     <>
-      <UncontrolledForm />
+      <ControlledForm />
     </>
   )
 }
