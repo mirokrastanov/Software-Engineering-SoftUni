d2f4f13ea554144d3260d4f47ba80ca6adfbfb75
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-12-11 21:38:25 +0200

    Create hook

---
+++ softuni-react-js/08-react-hooks/useForm2.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+
+export const useForm = (initialValues) => {
+    const [formValues, setFormValues] = useState(initialValues);
+
+    const onChangeHandler = (e) => {
+        setFormValues(state => ({ ...state, [e.target.name]: e.target.value }));
+
+    };
+
+    return {
+        formValues,
+        onChangeHandler,
+    }
+};
