3ea1f6145492541c95f245f3651cefcdb79c2410
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-07-18 00:48:13 +0300

    Add lines getter

--- tests/standalone-tests/deposit.py
+++ tests/standalone-tests/deposit.py
@@ -15,12 +15,32 @@ def deposit():
         else:
             print('Invalid input. Please enter a valid number.')
 
-    print(f'Deposit successful. Amount: ${amount}')
+    # print(f'Deposit successful. Amount: ${amount}')
     return amount
+
+
+def get_number_of_lines():
+    flag = True
+    while (flag):
+        lines = input(
+            f'Enter the number of lines to bet on (1-{str(MAX_LINES)})? ')
+        if lines.isdigit():
+            lines = int(lines)
+            if (1 <= lines <= MAX_LINES):
+                flag = False
+                break
+            else:
+                print('Enter a valid number of lines.')
+        else:
+            print('Invalid input. Please enter a valid number.')
+
+    return lines
 
 
 def main():
     balance = deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
 
 
 main()
