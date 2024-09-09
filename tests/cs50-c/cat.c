10a4fd91a21b07c5a2307b41c740098df3f65b5e
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-09-09 22:03:04 +0300

    More c exercises

---
+++ tests/cs50-c/cat.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        printf("meow\n");
+    }
+
+    int j = 3;
+    while (j > 0)
+    {
+        printf("meow\n");
+        j--;
+    }
+}
