5a4da1759847225c034ac87429543f9b805ead1f
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-12-06 16:51:13 +0200

    Setup metrics via prom-client

--- softuni-cloud-and-containers/07-app-monitoring/t4-1-contact-book-monitoring/controllers/mvc-controller.js
+++ softuni-cloud-and-containers/07-app-monitoring/t4-1-contact-book-monitoring/controllers/mvc-controller.js
@@ -1,25 +1,56 @@
+const client = requestAnimationFrame('prom-client');
+
+const register = new client.Registry();
+
+client.collectDefaultMetrics({
+  app: "node-application-monitoring-app",
+  prefix: "node_",
+  timeout: 10000,
+  gcDurationBuckets: [0.001, 0.01, 0.1, 1, 2, 5],
+  register,
+});
+
+const httpRequestTimer = new client.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in seconds',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [0.01, 0.03, 0.05, 0.07, 0.1, 0.3, 0.5, 0.7, 1, 2, 5, 10, 1000, 3000],
+});
+
+register.registerMetric(httpRequestTimer);
+
 function setup(app, data) {
-  app.get('/', function(req, res) {
+  app.get('/metrics', async (req, res) => {
+    const end = httpRequestTimer.startTimer();
+    const route = req.route.path;
+
+    res.setHeader('Content-type', register.contentType);
+    res.send(await register.metrics());
+
+    end({ route, code: res.statusCode, method: req.method });
+  })
+
+  app.get('/', function (req, res) {
     let contacts = data.getContacts();
     let model = { contacts };
     res.render('home', model);
   });
 
-  app.get('/contacts', function(req, res) {
+  app.get('/contacts', function (req, res) {
     let contacts = data.getContacts();
     let model = { contacts };
     res.render('contacts', model);
   });
 
-  app.get('/contacts/create', function(req, res) {
-      let model = {
-        firstName: "", lastName: "",
-        email: "", phone: "", comments: ""
-      };
-      res.render('create-contact', model);
+  app.get('/contacts/create', function (req, res) {
+    let model = {
+      firstName: "", lastName: "",
+      email: "", phone: "", comments: ""
+    };
+    res.render('create-contact', model);
   });
 
-  app.post('/contacts/create', function(req, res) {
+  app.post('/contacts/create', function (req, res) {
     let result = data.addContact(
       req.body.firstName, req.body.lastName,
       req.body.email, req.body.phone, req.body.comments);
@@ -36,7 +67,7 @@ function setup(app, data) {
     }
   });
 
-  app.get('/contacts/search', function(req, res) {
+  app.get('/contacts/search', function (req, res) {
     model = { keyword: "" };
     if (req.query.keyword) {
       model.keyword = req.query.keyword;
@@ -47,23 +78,25 @@ function setup(app, data) {
     res.render('search-contacts', model);
   });
 
-  app.get('/contacts/:id', function(req, res) {
+  app.get('/contacts/:id', function (req, res) {
     let contact = data.findContactById(req.params.id);
     if (contact.errMsg) {
-      model = {errText: 'Contact Not Found', 
-        errDetails: contact.errMsg};
+      model = {
+        errText: 'Contact Not Found',
+        errDetails: contact.errMsg
+      };
       res.render('error', model);
     }
-    else { 
+    else {
       let model = { contact };
-	    res.render('contact-details', model);
+      res.render('contact-details', model);
     }
   });
 
-  app.get('/contacts/find/:keyword', function(req, res) {
+  app.get('/contacts/find/:keyword', function (req, res) {
     let contacts = data.findContactByKeyword(req.params.keyword);
     let model = { contacts };
-	  res.render('contacts', model);
+    res.render('contacts', model);
   });
 }
 
